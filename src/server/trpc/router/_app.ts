8cae6a90fea68cb03a633eb464b73ce6cba1fd22
Michael Ongaro <35980684+michaelongaro@users.noreply.github.com>
2023-01-03 03:52:36 -0600

    - examples, + roomsRouter

--- src/server/trpc/router/_app.ts
+++ src/server/trpc/router/_app.ts
@@ -1,10 +1,8 @@
 import { router } from "../trpc";
-import { authRouter } from "./auth";
-import { exampleRouter } from "./example";
+import { roomsRouter } from "./rooms";
 
 export const appRouter = router({
-  example: exampleRouter,
-  auth: authRouter,
+  rooms: roomsRouter,
 });
 
 // export type definition of API
