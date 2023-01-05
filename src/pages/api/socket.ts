d2335db43d510da2db434407ceeccc467acc2c53
Michael Ongaro <35980684+michaelongaro@users.noreply.github.com>
2023-01-05 03:35:13 -0600

    type tweaks + game specific listeners + emits

--- src/pages/api/socket.ts
+++ src/pages/api/socket.ts
@@ -3,43 +3,58 @@ import {
   type IPlayerMetadata,
   type IRoomConfig,
 } from "../../components/CreateRoom/CreateRoom";
+import { type ICard } from "../../utils/generateDeckAndSqueakCards";
+import generateDeckAndSqueakCards from "../../utils/generateDeckAndSqueakCards";
 
 interface IRoomData {
-  [code: string]: IGameData;
+  [code: string]: IRoomMetadata;
 }
 
-interface IGameData {
-  game: IGameMetadata;
+interface IRoomMetadata {
   room: IRoomConfig;
   players: IPlayerMetadata[]; // may want to move over to an object to be able to access by userID
 }
 
-interface IGameMetadata {
-  board: IDeck[][]; // 2d array of cards
-  // player
+interface IGameData {
+  [code: string]: IGameMetadata;
 }
 
-interface IDeck {
-  cards: ICard[];
-  suit: number;
+export interface IGameMetadata {
+  board: (ICard | null)[][];
+  players: IPlayerCards;
 }
 
-interface ICard {
-  value: number;
-  suit: number;
+interface IPlayerCards {
+  [code: string]: IPlayerCardMetadata;
 }
 
-interface IRoomMetadata {
+interface IPlayerCardMetadata {
+  squeakPile: ICard[];
+  squeakRow: ICard[];
+  deck: ICard[];
+}
+
+// maybe use this again if you want to show some depth for each "deck" on board
+// instead of just showing the top card.
+// interface IDeck {
+//   cards: ICard[];
+//   suit: string;
+// }
+
+interface IJoinRoomConfig {
   code: string;
   username: string;
   userID: string;
 }
 
 const roomData: IRoomData = {};
+const gameData: IGameData = {};
 let numberOfPlayersReady = 0;
 
 // @ts-expect-error sdf
 export default function SocketHandler(req, res) {
+  console.log("server getting hit");
+
   // It means that socket server was already initialised
   if (res.socket.server.io) {
     console.log("Already set up");
@@ -64,15 +79,10 @@ export default function SocketHandler(req, res) {
     // look more into these methods
     // io.sockets.adapter.rooms.get(roomConfig.code)?.size
 
+    // room logic
     socket.on("createRoom", (roomConfig: IRoomConfig) => {
       socket.join(roomConfig.code);
       roomData[roomConfig.code] = {
-        // initialize 10x10 board here, (this obj will also hold player card data)
-        game: {
-          board: Array.from({ length: 10 }, () =>
-            Array.from({ length: 10 }, () => ({ cards: [], suit: 0 }))
-          ),
-        },
         room: roomConfig,
         players: [
           {
@@ -85,7 +95,7 @@ export default function SocketHandler(req, res) {
       io.in(roomConfig.code).emit("roomWasCreated");
     });
 
-    socket.on("joinRoom", (roomMetadata: IRoomMetadata) => {
+    socket.on("joinRoom", (roomMetadata: IJoinRoomConfig) => {
       socket.join(roomMetadata.code);
       roomData[roomMetadata.code]?.players.push({
         username: roomMetadata.username,
@@ -105,7 +115,7 @@ export default function SocketHandler(req, res) {
       // how to not have to extract it like this
       const currentPlayersInRoom =
         roomData[roomMetadata.code]?.room?.playersInRoom;
-      console.log("currentPlayersInRoom: ", currentPlayersInRoom);
+      // console.log("currentPlayersInRoom: ", currentPlayersInRoom);
       const updatedRoomConfig = {
         ...roomData[roomMetadata.code]?.room,
 
@@ -124,12 +134,47 @@ export default function SocketHandler(req, res) {
       io.in(roomCode).emit("navigateToPlayScreen")
     );
 
-    // look this over
-    socket.on("playerReady", (roomCode) => {
+    // game logic
+
+    socket.on("playerRejoinRoom", (roomCode) => {
+      console.log("playerRejoinRoom: ", roomCode);
+      socket.join(roomCode);
+    });
+
+    socket.on("playerReadyToReceiveInitGameData", (roomCode) => {
       numberOfPlayersReady++;
-      console.log("number of players ready: ", numberOfPlayersReady);
+      // console.log("number of players ready: ", numberOfPlayersReady);
+
+      if (numberOfPlayersReady !== roomData[roomCode]?.players.length) return;
+
+      const currentRoomPlayers = roomData[roomCode]?.players;
+      if (currentRoomPlayers) {
+        const board = Array.from({ length: 4 }, () =>
+          Array.from({ length: 5 }, () => null)
+        );
+
+        const playerCards: IPlayerCards = {};
+        // loop through players and create + get their cards
+        for (const player of currentRoomPlayers) {
+          playerCards[player.userID] = generateDeckAndSqueakCards();
+        }
+
+        gameData[roomCode] = {
+          board,
+          players: playerCards,
+        };
+      }
+
+      // console.log("gameData: ", gameData[roomCode]);
+
+      io.in(roomCode).emit("initGameData", gameData[roomCode]);
+      numberOfPlayersReady = 0;
+    });
+
+    socket.on("playerFullyReady", (roomCode) => {
+      numberOfPlayersReady++;
       if (numberOfPlayersReady === roomData[roomCode]?.players.length) {
-        io.in(roomCode).emit("startGame");
+        io.in(roomCode).emit("gameStarted");
         numberOfPlayersReady = 0;
       }
     });
