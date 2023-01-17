6bc2361877d71a1d20dcf02870d82da219939cac
Michael Ongaro <35980684+michaelongaro@users.noreply.github.com>
2023-01-16 20:33:10 -0600

    more uniform ids for targeting divs

--- src/components/Play/OtherPlayersCardContainers.tsx
+++ src/components/Play/OtherPlayersCardContainers.tsx
@@ -1,11 +1,27 @@
 import { useState, useEffect } from "react";
+import { socket } from "../../pages/";
 import { useLocalStorageContext } from "../../context/LocalStorageContext";
 import { useRoomContext } from "../../context/RoomContext";
-import { type IPlayerCardMetadata } from "../../pages/api/socket";
+import {
+  type IDrawFromSqueakDeck,
+  type ICardDropProposal,
+  type IPlayerCardMetadata,
+} from "../../pages/api/socket";
 import Card from "./Card";
 
 interface IOtherPlayersCardContainers {
   orderedClassNames: (string | undefined)[];
+}
+
+interface IOtherPlayers extends IPlayerCardMetadata {
+  playerID: string;
+  animationConfig: IAnimationConfig;
+}
+
+export interface IAnimationConfig {
+  xMultiplier: number;
+  yMultiplier: number;
+  rotation: number;
 }
 
 import classes from "./OtherPlayersCardContainers.module.css";
@@ -33,72 +49,164 @@ function OtherPlayersCardContainers({
   const userID = localStorageID.value; // change to ctx.userID ?? localStorageID.value
 
   const [allPlayersExceptCurrentUser, setAllPlayersExceptCurrentUser] =
-    useState<IPlayerCardMetadata[]>([]); // isn't this an array of IPlayerCardMetadata? aka
-  // { playerID: string}
-
-  // eventually add this listener
-
-  // socket.on("cardDropApproved", (data) => {
-  //   animate the card played from the player who played it to the board
-  // });
-
-  // import array of classnames
+    useState<IOtherPlayers[]>([]);
 
   useEffect(() => {
     if (roomCtx.gameData?.players) {
-      const playersToInclude: IPlayerCardMetadata[] = [];
-      for (const playerID of Object.keys(roomCtx.gameData.players)) {
+      const playersToInclude: IOtherPlayers[] = [];
+      let playerCounter = 0;
+      Object.keys(roomCtx.gameData.players).forEach((playerID) => {
         if (playerID !== userID) {
-          // can add playerID to obj below if you need it
+          // since each player's container is rotated, we need to adjust the
+          // polarities(?) of the x and y coordinates to make sure the cards are
+          // moving in the correct spot + card will need to be rotated correctly
+          let animationConfig: IAnimationConfig = {
+            xMultiplier: 1,
+            yMultiplier: 1,
+            rotation: 0,
+          };
+
+          if (playerCounter === 0) {
+            animationConfig = {
+              xMultiplier: -1,
+              yMultiplier: -1,
+              rotation: 180,
+            };
+          } else if (playerCounter === 1) {
+            animationConfig = { xMultiplier: 1, yMultiplier: -1, rotation: 90 };
+          } else if (playerCounter === 2) {
+            animationConfig = {
+              xMultiplier: -1,
+              yMultiplier: 1,
+              rotation: -90,
+            }; // or just 90?
+          }
+
           // @ts-expect-error asdf
-          playersToInclude.push(roomCtx.gameData.players[playerID]);
+          playersToInclude.push({
+            ...roomCtx.gameData.players[playerID],
+            playerID: playerID,
+            animationConfig: animationConfig,
+          });
+
+          playerCounter++;
         }
-      }
+      });
       setAllPlayersExceptCurrentUser(playersToInclude);
     }
   }, [roomCtx.gameData.players, userID]);
 
+  useEffect(() => {
+    allPlayersExceptCurrentUser.forEach((player) => {
+      console.log(
+        player.playerID,
+        player.topCardsInDeck[0],
+        player.topCardsInDeck[1],
+        player.topCardsInDeck[2]
+      );
+    });
+  }, [allPlayersExceptCurrentUser]);
+
   return (
     <>
       {allPlayersExceptCurrentUser.map((player, idx) => (
-        // I think probably a decent idea to add playerID to IPlayerCardMetadata right?
-        <div
-          key={idx} // change this key to playerID
-          className={orderedClassNames[idx]}
-        >
+        <div key={player.playerID} className={orderedClassNames[idx]}>
           <div className={internalOrderedGridClassNames[idx]}>
-            <div className={classes.squeakDeck}>
-              <Card showCardBack={true} draggable={false} />
+            <div
+              id={`${player.playerID}squeakDeck`}
+              className={`${classes.squeakDeck}  h-[64px] w-[48px] lg:h-[72px] lg:w-[56px]`}
+            >
+              {player.squeakDeck.length > 0 ? (
+                <div className="relative h-full w-full">
+                  <div className="absolute top-0 left-0 h-full w-full">
+                    <Card
+                      showCardBack={true}
+                      draggable={false}
+                      ownerID={player.playerID}
+                      startID={`${player.playerID}squeakDeck`}
+                      animationConfig={player.animationConfig}
+                    />
+                  </div>
+                  <div className="absolute top-0 left-0 h-full w-full">
+                    <Card
+                      value={player.squeakDeck[0]!.value}
+                      suit={player.squeakDeck[0]!.suit}
+                      showCardBack={true} // this would need to be changed halfway through card flip
+                      draggable={false}
+                      ownerID={player.playerID}
+                      startID={`${player.playerID}squeakDeck`}
+                      animationConfig={{
+                        xMultiplier: 1,
+                        yMultiplier: 1,
+                        rotation: 0,
+                      }}
+                    />
+                  </div>
+                </div>
+              ) : (
+                <button>Squeak!</button>
+              )}
             </div>
 
             {player.squeakHand.map((cards, cardsIdx) => (
               <div
-                key={`${userID}squeakStack${cardsIdx}`} // another great reason to add playerID to IPlayerCardMetadata
-                id={`${userID}SqueakHand${cardsIdx}`}
+                key={`${player.playerID}squeakStack${cardsIdx}`}
+                id={`${player.playerID}squeakHand${cardsIdx}`}
                 // @ts-expect-error asdf
-                className={`${cardClassMap[cardsIdx]} relative`}
+                className={`${cardClassMap[cardsIdx]} relative h-[64px] w-[48px] lg:h-[72px] lg:w-[56px]`}
               >
-                <>
+                <div
+                  style={{
+                    height:
+                      cards.length === 1 ? 72 : `${cards.length * 15 + 72}px`,
+                  }}
+                  className="absolute w-full"
+                >
                   {cards.map((card, cardIdx) => (
                     <div
-                      key={`${userID}card${cardIdx}`} // another great reason to add playerID to IPlayerCardMetadata
+                      key={`${player.playerID}card${cardIdx}`} //${card.suit}${card.value}
+                      id={`${player.playerID}squeakStack${cardsIdx}${cardIdx}`}
+                      className={`absolute left-0 h-full w-full`}
                       style={{
+                        // zIndex:
+                        //   roomCtx.originIndexForHeldSqueakCard === cardsIdx
+                        //     ? 501
+                        //     : "auto",
                         top: `${cardIdx * 15}px`,
                       }}
-                      className={`absolute left-0 h-full w-full`}
                     >
                       <Card
                         value={card.value}
                         suit={card.suit}
                         draggable={false}
+                        origin={"squeak"}
+                        ownerID={player.playerID}
+                        startID={`${player.playerID}squeakStack${cardsIdx}${cardIdx}`}
+                        animationConfig={player.animationConfig}
                       />
                     </div>
                   ))}
-                </>
+                </div>
               </div>
             ))}
 
-            <div className={classes.cardBeingPlayedOnBoard}>test!</div>
+            <div
+              className={classes.cardBeingPlayedOnBoard}
+              id={`${player.playerID}hand h-[64px] w-[48px] lg:h-[72px] lg:w-[56px]`}
+            >
+              <Card
+                value={player.topCardsInDeck?.[2]?.value}
+                suit={player.topCardsInDeck?.[2]?.suit}
+                showCardBack={false}
+                origin={"deck"} // probably only needed for current player
+                draggable={false}
+                ownerID={player.playerID}
+                startID={`${player.playerID}hand`}
+                animationConfig={player.animationConfig}
+              />
+            </div>
+
+            <div className={classes.playerAvatar}></div>
           </div>
         </div>
       ))}
