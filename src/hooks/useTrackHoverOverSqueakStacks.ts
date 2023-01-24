aec63717f459a4922856e18293ecaf3cc65847c5
Michael Ongaro <35980684+michaelongaro@users.noreply.github.com>
2023-01-23 21:40:16 -0600

    refactored to be sole handler of hoveredSqueakStack state

--- src/hooks/useTrackHoverOverSqueakStacks.ts
+++ src/hooks/useTrackHoverOverSqueakStacks.ts
@@ -9,208 +9,59 @@ function useTrackHoverOverSqueakStacks() {
 
   useEffect(() => {
     // mousemove listener to check which squeak stack is being hovered over
+    // excludes the squeak stack that the card is currently in
 
-    // TODO: refactor this with loops + logic rather than hardcoding
     function mouseHandler(e: MouseEvent) {
       if (userID === null) return;
 
-      const squeakHand0 = document.getElementById(`${userID}squeakHand0`);
-      const squeakHand1 = document.getElementById(`${userID}squeakHand1`);
-      const squeakHand2 = document.getElementById(`${userID}squeakHand2`);
-      const squeakHand3 = document.getElementById(`${userID}squeakHand3`);
+      const squeakHand0 = document
+        .getElementById(`${userID}squeakHand0`)
+        ?.getBoundingClientRect();
+      const squeakHand1 = document
+        .getElementById(`${userID}squeakHand1`)
+        ?.getBoundingClientRect();
+      const squeakHand2 = document
+        .getElementById(`${userID}squeakHand2`)
+        ?.getBoundingClientRect();
+      const squeakHand3 = document
+        .getElementById(`${userID}squeakHand3`)
+        ?.getBoundingClientRect();
 
-      if (roomCtx.originIndexForHeldSqueakCard === 0) {
-        if (squeakHand1) {
-          const rect = squeakHand1.getBoundingClientRect();
-
-          if (
-            e.clientX > rect.left &&
-            e.clientX < rect.right &&
-            e.clientY > rect.top &&
-            e.clientY < rect.bottom
-          ) {
-            roomCtx.setHoveredSqueakStack(1);
-          }
-        }
-
-        if (squeakHand2) {
-          const rect = squeakHand2.getBoundingClientRect();
-          if (
-            e.clientX > rect.left &&
-            e.clientX < rect.right &&
-            e.clientY > rect.top &&
-            e.clientY < rect.bottom
-          ) {
-            roomCtx.setHoveredSqueakStack(2);
-          }
-        }
-
-        if (squeakHand3) {
-          const rect = squeakHand3.getBoundingClientRect();
-          if (
-            e.clientX > rect.left &&
-            e.clientX < rect.right &&
-            e.clientY > rect.top &&
-            e.clientY < rect.bottom
-          ) {
-            roomCtx.setHoveredSqueakStack(3);
-          }
-        }
-      } else if (roomCtx.originIndexForHeldSqueakCard === 1) {
-        if (squeakHand0) {
-          const rect = squeakHand0.getBoundingClientRect();
-          if (
-            e.clientX > rect.left &&
-            e.clientX < rect.right &&
-            e.clientY > rect.top &&
-            e.clientY < rect.bottom
-          ) {
-            roomCtx.setHoveredSqueakStack(0);
-          }
-        }
-
-        if (squeakHand2) {
-          const rect = squeakHand2.getBoundingClientRect();
-          if (
-            e.clientX > rect.left &&
-            e.clientX < rect.right &&
-            e.clientY > rect.top &&
-            e.clientY < rect.bottom
-          ) {
-            roomCtx.setHoveredSqueakStack(2);
-          }
-        }
-
-        if (squeakHand3) {
-          const rect = squeakHand3.getBoundingClientRect();
-          if (
-            e.clientX > rect.left &&
-            e.clientX < rect.right &&
-            e.clientY > rect.top &&
-            e.clientY < rect.bottom
-          ) {
-            roomCtx.setHoveredSqueakStack(3);
-          }
-        }
-      } else if (roomCtx.originIndexForHeldSqueakCard === 2) {
-        if (squeakHand0) {
-          const rect = squeakHand0.getBoundingClientRect();
-          if (
-            e.clientX > rect.left &&
-            e.clientX < rect.right &&
-            e.clientY > rect.top &&
-            e.clientY < rect.bottom
-          ) {
-            roomCtx.setHoveredSqueakStack(0);
-          }
-        }
-
-        if (squeakHand1) {
-          const rect = squeakHand1.getBoundingClientRect();
-          if (
-            e.clientX > rect.left &&
-            e.clientX < rect.right &&
-            e.clientY > rect.top &&
-            e.clientY < rect.bottom
-          ) {
-            roomCtx.setHoveredSqueakStack(1);
-          }
-        }
-
-        if (squeakHand3) {
-          const rect = squeakHand3.getBoundingClientRect();
-          if (
-            e.clientX > rect.left &&
-            e.clientX < rect.right &&
-            e.clientY > rect.top &&
-            e.clientY < rect.bottom
-          ) {
-            roomCtx.setHoveredSqueakStack(3);
-          }
-        }
-      } else if (roomCtx.originIndexForHeldSqueakCard === 3) {
-        if (squeakHand0) {
-          const rect = squeakHand0.getBoundingClientRect();
-          if (
-            e.clientX > rect.left &&
-            e.clientX < rect.right &&
-            e.clientY > rect.top &&
-            e.clientY < rect.bottom
-          ) {
-            roomCtx.setHoveredSqueakStack(0);
-          }
-        }
-
-        if (squeakHand1) {
-          const rect = squeakHand1.getBoundingClientRect();
-          if (
-            e.clientX > rect.left &&
-            e.clientX < rect.right &&
-            e.clientY > rect.top &&
-            e.clientY < rect.bottom
-          ) {
-            roomCtx.setHoveredSqueakStack(1);
-          }
-        }
-
-        if (squeakHand2) {
-          const rect = squeakHand2.getBoundingClientRect();
-          if (
-            e.clientX > rect.left &&
-            e.clientX < rect.right &&
-            e.clientY > rect.top &&
-            e.clientY < rect.bottom
-          ) {
-            roomCtx.setHoveredSqueakStack(2);
-          }
-        }
-      } else if (roomCtx.holdingADeckCard) {
-        if (squeakHand0) {
-          const rect = squeakHand0.getBoundingClientRect();
-          if (
-            e.clientX > rect.left &&
-            e.clientX < rect.right &&
-            e.clientY > rect.top &&
-            e.clientY < rect.bottom
-          ) {
-            roomCtx.setHoveredSqueakStack(0);
-          }
-        }
-
-        if (squeakHand1) {
-          const rect = squeakHand1.getBoundingClientRect();
-          if (
-            e.clientX > rect.left &&
-            e.clientX < rect.right &&
-            e.clientY > rect.top &&
-            e.clientY < rect.bottom
-          ) {
-            roomCtx.setHoveredSqueakStack(1);
-          }
-        }
-
-        if (squeakHand2) {
-          const rect = squeakHand2.getBoundingClientRect();
-          if (
-            e.clientX > rect.left &&
-            e.clientX < rect.right &&
-            e.clientY > rect.top &&
-            e.clientY < rect.bottom
-          ) {
-            roomCtx.setHoveredSqueakStack(2);
-          }
-        }
-
-        if (squeakHand3) {
-          const rect = squeakHand3.getBoundingClientRect();
-          if (
-            e.clientX > rect.left &&
-            e.clientX < rect.right &&
-            e.clientY > rect.top &&
-            e.clientY < rect.bottom
-          ) {
-            roomCtx.setHoveredSqueakStack(3);
-          }
+      if (squeakHand0 && squeakHand1 && squeakHand2 && squeakHand3) {
+        if (
+          e.clientX > squeakHand0.left &&
+          e.clientX < squeakHand0.right &&
+          e.clientY > squeakHand0.top &&
+          e.clientY < squeakHand0.bottom &&
+          roomCtx.originIndexForHeldSqueakCard !== 0
+        ) {
+          roomCtx.setHoveredSqueakStack(0);
+        } else if (
+          e.clientX > squeakHand1.left &&
+          e.clientX < squeakHand1.right &&
+          e.clientY > squeakHand1.top &&
+          e.clientY < squeakHand1.bottom &&
+          roomCtx.originIndexForHeldSqueakCard !== 1
+        ) {
+          roomCtx.setHoveredSqueakStack(1);
+        } else if (
+          e.clientX > squeakHand2.left &&
+          e.clientX < squeakHand2.right &&
+          e.clientY > squeakHand2.top &&
+          e.clientY < squeakHand2.bottom &&
+          roomCtx.originIndexForHeldSqueakCard !== 2
+        ) {
+          roomCtx.setHoveredSqueakStack(2);
+        } else if (
+          e.clientX > squeakHand3.left &&
+          e.clientX < squeakHand3.right &&
+          e.clientY > squeakHand3.top &&
+          e.clientY < squeakHand3.bottom &&
+          roomCtx.originIndexForHeldSqueakCard !== 3
+        ) {
+          roomCtx.setHoveredSqueakStack(3);
+        } else {
+          roomCtx.setHoveredSqueakStack(null);
         }
       }
     }
