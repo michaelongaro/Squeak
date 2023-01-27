e3f5f9f2b1cc01de5bb6656529ff29b0b00dede7
Michael Ongaro <35980684+michaelongaro@users.noreply.github.com>
2023-01-27 04:04:23 -0600

    handles repeat rounds/new games properly

--- src/components/Play/Play.tsx
+++ src/components/Play/Play.tsx
@@ -10,6 +10,8 @@ import { socket } from "../../pages";
 import OtherPlayersCardContainers from "./OtherPlayersCardContainers";
 import Scoreboard from "../modals/Scoreboard/Scoreboard";
 import ShufflingCountdownModal from "../modals/ShufflingCountdownModal";
+import useStartAnotherRoundHandler from "../../hooks/useStartAnotherRoundHandler";
+import useReturnToRoomHandler from "../../hooks/useReturnToRoomHandler";
 
 function Play() {
   const roomCtx = useRoomContext();
@@ -18,15 +20,19 @@ function Play() {
   const userID = localStorageID.value; // change to ctx.userID ?? localStorageID.value
   const [gameStarted, setGameStarted] = useState<boolean>(false);
 
+  useStartAnotherRoundHandler();
+  useReturnToRoomHandler();
+
   useEffect(() => {
     if (
-      roomCtx.gameData.board === undefined &&
-      roomCtx.gameData.players === undefined
+      roomCtx.gameData?.board === undefined &&
+      roomCtx.gameData?.players === undefined
     ) {
+      roomCtx.setShowShufflingCountdown(true);
+
       socket.emit("playerReadyToReceiveInitGameData", roomCtx.roomConfig.code);
 
       socket.on("initGameData", (initGameData) => {
-        console.log("initGameData: ", initGameData);
         roomCtx.setGameData(initGameData);
         socket.emit("playerFullyReady", roomCtx.roomConfig.code);
       });
@@ -35,6 +41,16 @@ function Play() {
         setGameStarted(true);
       });
     }
+
+    return () => {
+      socket.off("initGameData", (initGameData) => {
+        roomCtx.setGameData(initGameData);
+        socket.emit("playerFullyReady", roomCtx.roomConfig.code);
+      });
+      socket.off("gameStarted", () => {
+        setGameStarted(true);
+      });
+    };
 
     // maybe you need to have a disconnect function that runs
     // when the component unmounts?
