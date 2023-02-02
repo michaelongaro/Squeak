caa552638e924b7435c7d12d0f04a4e99bbd70c0
Michael Ongaro <35980684+michaelongaro@users.noreply.github.com>
2023-02-01 19:27:19 -0600

    Scoreboard Refactor
    Uses <UserIcon />s for each respective user. Has a tiered coloring
    system to break up the sections for each user's section of the
    scoreboard. Extra timeouts for staggered animations. Uses newly added
    number animation library + confetti animation library.

--- src/components/modals/Scoreboard/Scoreboard.tsx
+++ src/components/modals/Scoreboard/Scoreboard.tsx
@@ -5,6 +5,9 @@ import { useRoomContext } from "../../../context/RoomContext";
 import useScoreboardData from "../../../hooks/useScoreboardData";
 import { IScoreboardMetadata } from "../../../pages/api/handlers/roundOverHandler";
 import AnimatedCardContainer from "./AnimatedCardContainer";
+import PlayerIcon from "../../playerIcons/PlayerIcon";
+import AnimatedNumber from "react-awesome-animated-number";
+import confetti from "canvas-confetti";
 
 interface IRanking {
   [key: number]: string;
@@ -17,8 +20,17 @@ const ranking: IRanking = {
   4: "4th",
 };
 
+interface IPlayerColorVariants {
+  [userID: string]: {
+    baseColor: string;
+    pointsBackgroundColor: string;
+    textColor: string;
+    animatedCardsBackgroundColor: string;
+  };
+}
+
 function Scoreboard() {
-  const { roomConfig, showScoreboard } = useRoomContext();
+  const { roomConfig, showScoreboard, playerMetadata } = useRoomContext();
 
   // const scoreboardData = useScoreboardData();
 
@@ -27,41 +39,110 @@ function Scoreboard() {
     useState<boolean>(false);
   const [showCountdownTimer, setShowCountdownTimer] = useState<boolean>(false);
 
+  const [animateCardsPlayedValue, setAnimateCardsPlayedValue] =
+    useState<boolean>(false);
+  const [animateSqueakModifierValue, setAnimateSqueakModifierValue] =
+    useState<boolean>(false);
+  const [animateTotalValue, setAnimateTotalValue] = useState<boolean>(true);
+  const [showConfetti, setShowConfetti] = useState<boolean>(false);
+
+  const [countdownTimerValue, setCountdownTimerValue] = useState<number>(3);
+
+  const [playerColorVariants, setPlayerColorVariants] =
+    useState<IPlayerColorVariants>({});
+
   // delete after testing
   const [onlyDoThisOnce, setOnlyDoThisOnce] = useState<boolean>(false);
 
   useEffect(() => {
     if (!onlyDoThisOnce && roomConfig.code) {
       setOnlyDoThisOnce(true);
+
+      setTimeout(() => {
+        setAnimateCardsPlayedValue(true);
+      }, 1000);
+
+      setTimeout(() => {
+        setAnimateSqueakModifierValue(true);
+      }, 2500);
+
+      setTimeout(() => {
+        setAnimateTotalValue(false); // when false, animates to new total value
+      }, 4000);
+
       setTimeout(() => {
         setShowNewRankings(true);
-      }, 3000);
+      }, 4000);
 
       setTimeout(() => {
         setShowWinningPlayerMessage(true);
       }, 6000);
 
       setTimeout(() => {
+        setShowConfetti(true);
+
+        confetti(
+          Object.assign(
+            {},
+            { origin: { x: 0.39, y: 0.72 } },
+            {
+              spread: 26,
+              startVelocity: 35,
+              angle: 135,
+              zIndex: 999,
+            },
+            {
+              particleCount: 100,
+            }
+          )
+        );
+
+        confetti(
+          Object.assign(
+            {},
+            { origin: { x: 0.6, y: 0.72 } },
+            {
+              spread: 26,
+              startVelocity: 35,
+              angle: 45,
+              zIndex: 999,
+            },
+            {
+              particleCount: 100,
+            }
+          )
+        );
+      }, 6500);
+
+      setTimeout(() => {
         setShowCountdownTimer(true);
+
+        setTimeout(() => {
+          setCountdownTimerValue(2);
+        }, 2000);
+
+        setTimeout(() => {
+          setCountdownTimerValue(1);
+        }, 3000);
       }, 9000);
 
       // delete this later
-      setTimeout(() => {
-        socket.emit("resetGame", {
-          roomCode: roomConfig.code,
-          gameIsFinished: true,
-        });
-      }, 15000);
+      // setTimeout(() => {
+      //   socket.emit("resetGame", {
+      //     roomCode: roomConfig.code,
+      //     gameIsFinished: true,
+      //   });
+      // }, 15000);
     }
   }, [roomConfig.code, onlyDoThisOnce]);
 
   const scoreboardData: Partial<IScoreboardMetadata> | null = {
     gameWinnerID: null,
-    roundWinnerID: "26d44c9f4211a557",
+    roundWinnerID: "cldk02mcr0000ul6k95niugk9",
     playerRoundDetails: [
       {
-        playerID: "26d44c9f4211a557",
-        oldScore: 0,
+        playerID: "cldk02mcr0000ul6k95niugk9",
+        oldScore: 10,
         newScore: 25,
         oldRanking: 1,
         newRanking: 2,
@@ -123,7 +204,7 @@ function Scoreboard() {
       },
       {
         playerID: "8ea9ee59061148a0",
-        oldScore: 0,
+        oldScore: 15,
         newScore: 2,
         oldRanking: 1,
         newRanking: 3,
@@ -162,6 +243,33 @@ function Scoreboard() {
     ],
   };
 
+  useEffect(() => {
+    if (Object.keys(playerColorVariants).length !== 0) return;
+
+    const newPlayerColorVariants: IPlayerColorVariants = {};
+
+    for (const userID in playerMetadata) {
+      const hslColor = playerMetadata[userID]?.color;
+
+      if (!hslColor) return;
+
+      const strippedHSLColor = hslColor.slice(0, -4);
+      const lightness = parseInt(
+        hslColor.slice(hslColor.length - 4, hslColor.length).trim()
+      );
+
+      newPlayerColorVariants[userID] = {
+        baseColor: hslColor,
+        pointsBackgroundColor:
+          strippedHSLColor + `${Math.floor(lightness * 0.75)}%)`,
+        textColor: strippedHSLColor + `${Math.floor(lightness * 1.5)}%)`,
+        animatedCardsBackgroundColor: strippedHSLColor + "90%)", //`${Math.floor(lightness * 1)}%)`
+      };
+    }
+
+    setPlayerColorVariants(newPlayerColorVariants);
+  }, [playerMetadata, playerColorVariants]);
+
   // have some kind of delay that will immediately have the data rendered below but still have
   // the opacity/pointer events of main modal be 0/none for a few seconds while squeak animation is playing
 
@@ -173,9 +281,9 @@ function Scoreboard() {
       }}
       className="baseFlex absolute top-0 left-0 z-[600] h-full w-full bg-black bg-opacity-60 transition-all"
     >
-      <div className="h-[60%] w-[65%] rounded-lg bg-green-200 p-4">
+      <div className="h-[75%] w-[75%] rounded-lg bg-green-200 p-4">
         {scoreboardData?.playerRoundDetails && (
-          <div className="baseVertFlex h-full gap-4">
+          <div className="baseVertFlex h-full gap-12">
             <div className="text-2xl">Scoreboard</div>
 
             <div className="baseFlex h-full w-full gap-4">
@@ -185,27 +293,115 @@ function Scoreboard() {
                   className="baseVertFlex h-full w-full rounded-md border-2 border-black shadow-md"
                 >
                   {/* avatar + username */}
-                  <div className="baseFlex w-full gap-2 border-b-2 border-black">
-                    <div>avatar</div>
-                    {player.playerID}
+                  <div
+                    style={{
+                      backgroundColor:
+                        playerColorVariants[player.playerID]?.baseColor ??
+                        "white",
+                    }}
+                    className="baseVertFlex h-36 w-full gap-2 border-b-2 border-black"
+                  >
+                    <PlayerIcon
+                      avatarPath={
+                        playerMetadata[player.playerID]?.avatarPath ??
+                        "/avatars/rabbit.svg"
+                      }
+                      borderColor={
+                        playerMetadata[player.playerID]?.color ??
+                        "hsl(352deg, 69%, 61%)"
+                      }
+                      size={"3rem"}
+                    />
+                    <div
+                      style={{
+                        color:
+                          playerColorVariants[player.playerID]?.textColor ??
+                          "black",
+                      }}
+                    >
+                      {playerMetadata[player.playerID]?.username}
+                    </div>
                   </div>
 
                   {/* anim. scores + cards */}
-                  <div className="relative z-[1] h-full w-full overflow-hidden">
+                  <div
+                    style={{
+                      backgroundColor:
+                        playerColorVariants[player.playerID]
+                          ?.animatedCardsBackgroundColor ?? "white",
+                      color:
+                        playerColorVariants[player.playerID]?.textColor ??
+                        "black",
+                    }}
+                    className="relative z-[1] h-full w-full overflow-hidden"
+                  >
                     {/* ideally some kind of glassmorphism  */}
-                    <div className="baseVertFlex absolute top-0 left-0 z-[3] w-full bg-black bg-opacity-30 p-2">
-                      <div className="align-center flex w-full justify-between  pl-8 pr-8 text-lg">
-                        Cards played <CountUp end={player.cardsPlayed.length} />
+                    <div
+                      style={{
+                        backgroundColor:
+                          playerColorVariants[player.playerID]
+                            ?.pointsBackgroundColor ?? "white",
+                      }}
+                      className="baseVertFlex absolute top-0 left-0 z-[3] w-full bg-black bg-opacity-30 p-2"
+                    >
+                      <div className="align-center flex w-full justify-between pl-8 pr-8 text-lg ">
+                        Cards played
+                        <div className="baseFlex">
+                          <div
+                            style={{
+                              opacity: animateCardsPlayedValue ? 1 : 0,
+                            }}
+                            className="transition-opacity"
+                          >
+                            +
+                          </div>
+                          <AnimatedNumber
+                            value={
+                              animateCardsPlayedValue
+                                ? player.cardsPlayed.length
+                                : 0
+                            }
+                            duration={1500}
+                            hasComma={true}
+                            size={18}
+                          />
+                        </div>
                       </div>
                       {/* make this red/green text */}
-                      <div className="align-center flex w-full justify-between  pl-8 pr-8 text-lg">
-                        Squeak <CountUp end={player.squeakModifier} />
+                      <div className="align-center flex w-full justify-between pl-8 pr-8 text-lg">
+                        Squeak
+                        <div className="baseFlex">
+                          <div
+                            style={{
+                              opacity: animateSqueakModifierValue ? 1 : 0,
+                            }}
+                            className="transition-opacity"
+                          >
+                            {player.squeakModifier > 0 ? "+" : ""}
+                          </div>
+                          <AnimatedNumber
+                            value={
+                              animateSqueakModifierValue
+                                ? player.squeakModifier
+                                : 0
+                            }
+                            duration={1500}
+                            hasComma={true}
+                            size={18}
+                          />
+                        </div>
                       </div>
-                      <div className="align-center flex w-full justify-between pl-8  pr-8 text-xl">
-                        Total{" "}
-                        <CountUp
-                          start={player.oldScore}
-                          end={player.newScore}
+                      <div className="align-center flex w-full justify-between pl-8 pr-8 text-xl">
+                        Total
+                        <AnimatedNumber
+                          value={
+                            animateTotalValue
+                              ? player.oldScore
+                              : player.newScore
+                          }
+                          duration={1500}
+                          hasComma={true}
+                          size={20}
                         />
                       </div>
                     </div>
@@ -220,7 +416,17 @@ function Scoreboard() {
                   {/* currently thinking to have this area and avatar+username area on top to be a slightly
                       lighter (filter: brightness(1.1)?, so that the avatar background can still be seen easily)
                       (prob still want 1px black border around avatar), and then just have an obj for   */}
-                  <div className="baseFlex w-full gap-2 rounded-b-md border-t-2 border-black p-2">
+                  <div
+                    style={{
+                      backgroundColor:
+                        playerColorVariants[player.playerID]?.baseColor ??
+                        "white",
+                      color:
+                        playerColorVariants[player.playerID]?.textColor ??
+                        "black",
+                    }}
+                    className="baseFlex w-full gap-2 rounded-b-md border-t-2 border-black p-2"
+                  >
                     {showNewRankings
                       ? ranking[player.newRanking]
                       : ranking[player.oldRanking]}
@@ -235,24 +441,78 @@ function Scoreboard() {
               style={{
                 opacity: showWinningPlayerMessage ? 1 : 0,
                 pointerEvents: showWinningPlayerMessage ? "auto" : "none",
+                backgroundColor:
+                  playerColorVariants[scoreboardData.roundWinnerID!]
+                    ?.baseColor ?? "black",
               }}
-              className="text-xl transition-all"
+              className="baseFlex gap-4 rounded-md p-4 transition-all"
             >
-              Avatar PlayerWhoWon won the round!
+              {/* left confetti cannon */}
+              <img
+                style={{
+                  opacity: showConfetti ? 1 : 0,
+                  transform: showConfetti
+                    ? "scale(1) rotate(135deg)"
+                    : "scale(0) rotate(135deg)",
+                  filter: "drop-shadow(rgba(0,0,0, 0.15) 0px 0px 0.5rem)",
+                }}
+                className="h-8 w-8 transition-all"
+                src="/scoreboard/confettiCannon.svg"
+                alt={"left celebratory confetti cannon"}
+              />
+              {/* remove "!"s when using actual hook */}
+              <PlayerIcon
+                avatarPath={
+                  playerMetadata[scoreboardData.roundWinnerID!]?.avatarPath ??
+                  "/avatars/rabbit.svg"
+                }
+                borderColor={
+                  playerMetadata[scoreboardData.roundWinnerID!]?.color ??
+                  "hsl(352deg, 69%, 61%)"
+                }
+                size={"3rem"}
+              />
+              <div
+                style={{
+                  color:
+                    playerColorVariants[scoreboardData.roundWinnerID!]
+                      ?.textColor ?? "black",
+                }}
+                className="text-xl"
+              >
+                {playerMetadata[scoreboardData.roundWinnerID!]?.username}
+                {` won the ${scoreboardData.gameWinnerID ? "game" : "round"}!`}
+              </div>
+              {/* right confetti cannon */}
+              <img
+                style={{
+                  opacity: showConfetti ? 1 : 0,
+                  transform: showConfetti
+                    ? "scale(1) rotate(225deg)"
+                    : "scale(0) rotate(225deg)",
+                  filter: "drop-shadow(rgba(0,0,0, 0.15) 0px 0px 0.5rem)",
+                }}
+                className="h-8 w-8 transition-all"
+                src="/scoreboard/confettiCannon.svg"
+                alt={"right celebratory confetti cannon"}
+              />
             </div>
-            {/* have this fade in after message above has been shown, with timeouts */}
+
             <div
               style={{
                 opacity: showCountdownTimer ? 1 : 0,
                 pointerEvents: showCountdownTimer ? "auto" : "none",
               }}
-              className="baseFlex gap-2 transition-all"
+              className="baseFlex gap-2 text-lg transition-all"
             >
               <div>Next round starts in:</div>
-              <div>
-                <CountUp start={5} end={1} />
-                ...
-              </div>
+
+              <AnimatedNumber
+                value={countdownTimerValue}
+                duration={1500}
+                hasComma={true}
+                size={20}
+              />
             </div>
           </div>
         )}
