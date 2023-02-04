77fd982b92076f617ff6d701be1e2f0fbb4eed5b
Michael Ongaro <35980684+michaelongaro@users.noreply.github.com>
2023-02-04 03:35:59 -0600

    1st runthrough of skeleton animation

--- src/components/playerIcons/PlayerIcon.tsx
+++ src/components/playerIcons/PlayerIcon.tsx
@@ -1,39 +1,42 @@
 import React from "react";
 
 interface IPlayerIcon {
-  avatarPath: string;
-  borderColor: string;
+  avatarPath?: string;
+  borderColor?: string;
   size: string;
   username?: string;
 }
 // TODO: probably just pass through the whole player object and allow it to be undefined,
-// if so then return null
-
-// probably need to add opacity as a prop because you don't always want the bg-opacity-80 (tooltip)
+// if so then just show loading skeleton?
 
 function PlayerIcon({ avatarPath, borderColor, size, username }: IPlayerIcon) {
   return (
-    <div className="baseVertFlex gap-2">
-      <div
-        style={{
-          outline: `4px solid ${borderColor}`,
-        }}
-        className="rounded-full bg-white bg-opacity-80"
-        onClick={() => console.log("child clicked")}
-      >
-        <img
-          style={{
-            width: size,
-            height: size,
-          }}
-          className="p-2"
-          src={avatarPath}
-          alt={"Player Icon"}
-          draggable="false"
-        />
-      </div>
-      {username ? username : null}
-    </div>
+    <>
+      {avatarPath && borderColor ? (
+        <div className="baseVertFlex gap-2">
+          <div
+            style={{
+              outline: `4px solid ${borderColor}`,
+            }}
+            className="rounded-full bg-white bg-opacity-80"
+          >
+            <img
+              style={{
+                width: size,
+                height: size,
+              }}
+              className="p-2"
+              src={avatarPath}
+              alt={"Player Icon"}
+              draggable="false"
+            />
+          </div>
+          {username ? username : null}
+        </div>
+      ) : (
+        <div className="skeletonLoading h-12 w-12 rounded-full"></div>
+      )}
+    </>
   );
 }
 
