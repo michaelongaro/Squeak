26ee018f49a4b7a38530958354488339d0fa11fb
Michael Ongaro <35980684+michaelongaro@users.noreply.github.com>
2023-02-04 03:32:17 -0600

    added playerIcon + username if user logged in

--- src/components/MainOptions/MainOptions.tsx
+++ src/components/MainOptions/MainOptions.tsx
@@ -10,47 +10,73 @@ import {
   AiOutlinePlusCircle,
   AiOutlineInfoCircle,
 } from "react-icons/ai";
+import { useSession } from "next-auth/react";
+import PlayerIcon from "../playerIcons/PlayerIcon";
+import { useUserIDContext } from "../../context/UserIDContext";
+import { trpc } from "../../utils/trpc";
 
 function MainOptions() {
+  const { data: session, status } = useSession();
+  const { value: userID } = useUserIDContext();
+
+  // probably want to remove the default "refetch on page focus" behavior
+  const { data: user } = trpc.users.getUserByID.useQuery(userID);
   const { setPageToRender } = useRoomContext();
 
   const [showTutorialModal, setShowTutorialModal] = useState<boolean>(false);
 
   return (
     <div className="baseFlex relative min-h-[100vh]">
-      <div className="baseVertFlex gap-8 rounded-md border-2 border-white bg-green-800 p-8 shadow-lg">
-        <div className="text-4xl text-green-300 sm:text-5xl">Squeak</div>
+      {status !== "loading" && (
+        <div className="baseVertFlex min-w-[22.25rem] gap-8 rounded-md border-2 border-white bg-green-800 p-8 shadow-lg">
+          <div className="text-4xl text-green-300 sm:text-5xl">Squeak</div>
 
-        <LogIn gap={"2rem"} />
+          {status === "authenticated" ? (
+            <div className="baseFlex gap-4">
+              <PlayerIcon
+                avatarPath={user?.avatarPath}
+                borderColor={user?.color}
+                size={"3rem"}
+              />
+              {user?.username ? (
+                <div className="text-green-300">{user?.username}</div>
+              ) : (
+                <div className="skeletonLoading h-6 w-28 rounded-sm"></div>
+              )}
+            </div>
+          ) : (
+            <LogIn gap={"2rem"} />
+          )}
 
-        <div className="baseVertFlex gap-4">
-          <SecondaryButton
-            innerText={"How to play"}
-            icon={<AiOutlineInfoCircle size={"1.5rem"} />}
-            iconOnLeft={true}
-            extraPadding={true}
-            onClickFunction={() => setShowTutorialModal(true)}
-          />
+          <div className="baseVertFlex gap-4">
+            <SecondaryButton
+              innerText={"How to play"}
+              icon={<AiOutlineInfoCircle size={"1.5rem"} />}
+              iconOnLeft={true}
+              extraPadding={true}
+              onClickFunction={() => setShowTutorialModal(true)}
+            />
 
-          <SecondaryButton
-            innerText={"Create room"}
-            icon={<AiOutlinePlusCircle size={"1.5rem"} />}
-            iconOnLeft={true}
-            extraPadding={true}
-            onClickFunction={() => setPageToRender("createRoom")}
-          />
+            <SecondaryButton
+              innerText={"Create room"}
+              icon={<AiOutlinePlusCircle size={"1.5rem"} />}
+              iconOnLeft={true}
+              extraPadding={true}
+              onClickFunction={() => setPageToRender("createRoom")}
+            />
 
-          <SecondaryButton
-            innerText={"Join room"}
-            icon={<ImEnter size={"1.5rem"} />}
-            iconOnLeft={true}
-            extraPadding={true}
-            onClickFunction={() => setPageToRender("joinRoom")}
-          />
+            <SecondaryButton
+              innerText={"Join room"}
+              icon={<ImEnter size={"1.5rem"} />}
+              iconOnLeft={true}
+              extraPadding={true}
+              onClickFunction={() => setPageToRender("joinRoom")}
+            />
 
-          {/* leaderboard */}
+            {/* leaderboard */}
+          </div>
         </div>
-      </div>
+      )}
 
       {showTutorialModal && <TutorialModal />}
 
