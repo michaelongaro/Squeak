1899b82092ca3231d15538c2f2ec1b7879e4acee
Michael Ongaro <35980684+michaelongaro@users.noreply.github.com>
2023-02-08 22:06:49 -0600

    sets ctx state based on emitted socket data

--- src/hooks/useReturnToRoomHandler.ts
+++ src/hooks/useReturnToRoomHandler.ts
@@ -2,15 +2,20 @@ import { useState, useEffect } from "react";
 import { useUserIDContext } from "../context/UserIDContext";
 import { useRoomContext } from "../context/RoomContext";
 import { socket } from "../pages";
-import { type IGameMetadata } from "../pages/api/socket";
+import { type IMoveBackToLobby } from "../pages/api/socket";
 
 function useReturnToRoomHandler() {
-  const { roomConfig, setPageToRender } = useRoomContext();
+  const {
+    roomConfig,
+    setPageToRender,
+    setRoomConfig,
+    setPlayerMetadata,
+    setGameData,
+  } = useRoomContext();
   const { value: userID } = useUserIDContext();
 
-  const [dataFromBackend, setDataFromBackend] = useState<IGameMetadata | null>(
-    null
-  );
+  const [dataFromBackend, setDataFromBackend] =
+    useState<IMoveBackToLobby | null>(null);
 
   useEffect(() => {
     socket.on("moveBackToLobby", (data) => setDataFromBackend(data));
@@ -24,13 +29,27 @@ function useReturnToRoomHandler() {
     if (dataFromBackend !== null) {
       setDataFromBackend(null);
 
-      if (userID === roomConfig.hostUserID) {
+      const { roomConfig: newRoomConfig, players, gameData } = dataFromBackend;
+
+      setRoomConfig(newRoomConfig);
+      setPlayerMetadata(players);
+      setGameData(gameData);
+
+      if (userID === newRoomConfig.hostUserID) {
         setPageToRender("createRoom");
       } else {
         setPageToRender("joinRoom");
       }
     }
-  }, [dataFromBackend, setPageToRender, roomConfig.hostUserID, userID]);
+  }, [
+    dataFromBackend,
+    setPageToRender,
+    roomConfig.hostUserID,
+    userID,
+    setGameData,
+    setPlayerMetadata,
+    setRoomConfig,
+  ]);
 }
 
 export default useReturnToRoomHandler;
