85b4f845b40cba18f196141c3d0cbf0842e968c9
Michael Ongaro <35980684+michaelongaro@users.noreply.github.com>
2023-02-08 22:04:16 -0600

    usePlayerLeftRoom hook + topControls now just in this component

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -1,25 +1,31 @@
-import { useEffect, useState } from "react";
 import CreateRoom from "../CreateRoom/CreateRoom";
 import JoinRoom from "../JoinRoom/JoinRoom";
 import Play from "../Play/Play";
 import { useRoomContext } from "../../context/RoomContext";
 import MainOptions from "../MainOptions/MainOptions";
 import { AnimatePresence } from "framer-motion";
+import TopRightControls from "../TopRightControls/TopRightControls";
+import usePlayerLeftRoom from "../../hooks/usePlayerLeftRoom";
 
 function HomePage() {
   const { connectedToRoom, pageToRender } = useRoomContext();
 
+  usePlayerLeftRoom();
+
   return (
+    <div className="relative">
       <AnimatePresence
         initial={false}
         mode={"wait"}
         onExitComplete={() => null}
       >
-      {pageToRender === "home" && <MainOptions />}
-      {pageToRender === "createRoom" && <CreateRoom />}
-      {pageToRender === "joinRoom" && <JoinRoom />}
-      {pageToRender === "play" && connectedToRoom && <Play />}
-    </AnimatePresence>
+        {pageToRender === "home" && <MainOptions />}
+        {pageToRender === "createRoom" && <CreateRoom />}
+        {pageToRender === "joinRoom" && <JoinRoom />}
+        {pageToRender === "play" && connectedToRoom && <Play />}
+      </AnimatePresence>
+      <TopRightControls forPlayScreen={pageToRender === "play"} />
+    </div>
   );
 }
 
