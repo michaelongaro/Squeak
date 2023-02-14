90964f5444a5ea33623befbe437c5cca83b32a8f
Michael Ongaro <35980684+michaelongaro@users.noreply.github.com>
2023-02-13 22:59:35 -0600

    addFriend + kickPlayer buttons

--- src/components/playerIcons/PlayerIcon.tsx
+++ src/components/playerIcons/PlayerIcon.tsx
@@ -1,15 +1,35 @@
 import React from "react";
+import { socket } from "../../pages";
+import SecondaryButton from "../Buttons/SecondaryButton";
+import { AiOutlinePlus, AiOutlineClose } from "react-icons/ai";
+import DangerButton from "../Buttons/DangerButton";
+import { useRoomContext } from "../../context/RoomContext";
+import { useUserIDContext } from "../../context/UserIDContext";
 
 interface IPlayerIcon {
   avatarPath?: string;
   borderColor?: string;
   size: string;
   username?: string;
+  playerID?: string;
+  showAddFriendButton?: boolean;
+  showRemovePlayerFromRoomButton?: boolean;
 }
 // TODO: probably just pass through the whole player object and allow it to be undefined,
 // if so then just show loading skeleton?
 
-function PlayerIcon({ avatarPath, borderColor, size, username }: IPlayerIcon) {
+function PlayerIcon({
+  avatarPath,
+  borderColor,
+  size,
+  username,
+  showAddFriendButton,
+  playerID,
+  showRemovePlayerFromRoomButton,
+}: IPlayerIcon) {
+  const { roomConfig } = useRoomContext();
+  const { value: userID } = useUserIDContext();
+
   return (
     <>
       {avatarPath && borderColor ? (
@@ -23,7 +43,7 @@ function PlayerIcon({ avatarPath, borderColor, size, username }: IPlayerIcon) {
             style={{
               outline: `4px solid ${borderColor}`,
             }}
-            className="rounded-full bg-white bg-opacity-80"
+            className="relative rounded-full bg-white bg-opacity-80"
           >
             <img
               style={{
@@ -35,6 +55,54 @@ function PlayerIcon({ avatarPath, borderColor, size, username }: IPlayerIcon) {
               alt={"Player Icon"}
               draggable="false"
             />
+
+            {showAddFriendButton && (
+              <SecondaryButton
+                icon={<AiOutlinePlus size={"1rem"} />}
+                extraPadding={false}
+                width={"30px"}
+                height={"30px"}
+                onClickFunction={() => {
+                  socket.emit("modifyFriendData", {
+                    action: "sendFriendInvite",
+                    initiatorID: userID,
+                    targetID: playerID,
+                  });
+                }}
+                style={{
+                  position: "absolute",
+                  top: "-0.75rem",
+                  left: "-1rem",
+                  padding: "0",
+                  borderRadius: "50%",
+                }}
+              />
+            )}
+
+            {showRemovePlayerFromRoomButton && (
+              <DangerButton
+                innerText="Confirm"
+                innerTooltipText="Kick player?"
+                relativeTooltipPosition="bottom"
+                icon={<AiOutlineClose size={"1rem"} />}
+                onClickFunction={() => {
+                  if (!playerID) return;
+                  socket.emit("leaveRoom", {
+                    playerID,
+                    roomCode: roomConfig.code,
+                  });
+                }}
+                width={"30px"}
+                height={"30px"}
+                style={{
+                  position: "absolute",
+                  top: "-0.75rem",
+                  right: "-1rem",
+                  padding: "0",
+                  borderRadius: "50%",
+                }}
+              />
+            )}
           </div>
           {username ? username : null}
         </div>
