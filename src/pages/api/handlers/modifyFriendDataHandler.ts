39c4c146a2af55092ba6a60ca5cb097203e95aee
Michael Ongaro <35980684+michaelongaro@users.noreply.github.com>
2023-02-14 19:28:47 -0600

    added extra handlers to match all possible actions

--- src/pages/api/handlers/modifyFriendDataHandler.ts
+++ src/pages/api/handlers/modifyFriendDataHandler.ts
@@ -11,6 +11,7 @@ export function modifyFriendDataHandler(
     initiatorID,
     targetID,
     roomCode,
+    currentRoomIsPublic,
   }: IModifyFriendData) {
     const initiator = friendData[initiatorID];
     const target = targetID ? friendData[targetID] : null;
@@ -20,14 +21,16 @@ export function modifyFriendDataHandler(
     if (action === "sendFriendInvite" && targetID && target) {
       target.friendInviteIDs.push(initiatorID);
 
-      prisma.user.update({
-        where: {
-          id: targetID,
-        },
-        data: {
-          friendInviteIDs: target.friendInviteIDs,
-        },
-      });
+      prisma.user
+        .update({
+          where: {
+            id: targetID,
+          },
+          data: {
+            friendInviteIDs: target.friendInviteIDs,
+          },
+        })
+        .catch((err) => console.log(err));
 
       io.emit("friendDataUpdated", {
         playerID: targetID,
@@ -41,24 +44,28 @@ export function modifyFriendDataHandler(
         (id) => id !== targetID
       );
 
-      prisma.user.update({
-        where: {
-          id: initiatorID,
-        },
-        data: {
-          friendIDs: initiator.friendIDs,
-          friendInviteIDs: initiator.friendInviteIDs,
-        },
-      });
+      prisma.user
+        .update({
+          where: {
+            id: initiatorID,
+          },
+          data: {
+            friendIDs: initiator.friendIDs,
+            friendInviteIDs: initiator.friendInviteIDs,
+          },
+        })
+        .catch((err) => console.log(err));
 
-      prisma.user.update({
-        where: {
-          id: targetID,
-        },
-        data: {
-          friendIDs: target.friendIDs,
-        },
-      });
+      prisma.user
+        .update({
+          where: {
+            id: targetID,
+          },
+          data: {
+            friendIDs: target.friendIDs,
+          },
+        })
+        .catch((err) => console.log(err));
 
       io.emit("friendDataUpdated", {
         playerID: initiatorID,
@@ -69,15 +76,245 @@ export function modifyFriendDataHandler(
         playerID: targetID,
         friendData: target,
       });
-    } else if ((action === "createRoom" || action === "joinRoom") && roomCode) {
-      prisma.user.update({
-        where: {
-          id: initiatorID,
-        },
-        data: {
-          roomCode: roomCode,
-        },
+    } else if (action === "declineFriendInvite") {
+      initiator.friendInviteIDs = initiator.friendInviteIDs.filter(
+        (id) => id !== targetID
+      );
+
+      prisma.user
+        .update({
+          where: {
+            id: initiatorID,
+          },
+          data: {
+            friendInviteIDs: initiator.friendInviteIDs,
+          },
+        })
+        .catch((err) => console.log(err));
+
+      io.emit("friendDataUpdated", {
+        playerID: initiatorID,
+        friendData: initiator,
       });
+    } else if (action === "sendRoomInvite" && targetID && target) {
+      target.roomInviteIDs.push(initiatorID);
+
+      prisma.user
+        .update({
+          where: {
+            id: targetID,
+          },
+          data: {
+            roomInviteIDs: target.roomInviteIDs,
+          },
+        })
+        .catch((err) => console.log(err));
+
+      io.emit("friendDataUpdated", {
+        playerID: targetID,
+        friendData: target,
+      });
+    } else if (
+      action === "acceptRoomInvite" &&
+      targetID &&
+      target &&
+      roomCode !== undefined &&
+      currentRoomIsPublic !== undefined
+    ) {
+      initiator.roomInviteIDs = initiator.roomInviteIDs.filter(
+        (id) => id !== targetID
+      );
+
+      prisma.user
+        .update({
+          where: {
+            id: initiatorID,
+          },
+          data: {
+            roomInviteIDs: initiator.roomInviteIDs,
+            status: "in a room",
+            roomCode: roomCode,
+            currentRoomIsPublic: currentRoomIsPublic,
+          },
+        })
+        .catch((err) => console.log(err));
+
+      for (const friendID of initiator.friendIDs) {
+        const friend = friendData[friendID];
+
+        if (!friend) continue; // don't think this should ever happen but continue better than return here?
+
+        io.emit("friendDataUpdated", {
+          playerID: friendID,
+          friendData: friend,
+        });
+      }
+    } else if (action === "declineRoomInvite") {
+      initiator.roomInviteIDs = initiator.roomInviteIDs.filter(
+        (id) => id !== targetID
+      );
+
+      prisma.user
+        .update({
+          where: {
+            id: initiatorID,
+          },
+          data: {
+            roomInviteIDs: initiator.roomInviteIDs,
+          },
+        })
+        .catch((err) => console.log(err));
+
+      io.emit("friendDataUpdated", {
+        playerID: initiatorID,
+        friendData: initiator,
+      });
+    } else if (action === "removeFriend" && targetID && target) {
+      initiator.friendIDs = initiator.friendIDs.filter((id) => id !== targetID);
+
+      target.friendIDs = target.friendIDs.filter((id) => id !== initiatorID);
+
+      prisma.user
+        .update({
+          where: {
+            id: initiatorID,
+          },
+          data: {
+            friendIDs: initiator.friendIDs,
+          },
+        })
+        .catch((err) => console.log(err));
+
+      prisma.user
+        .update({
+          where: {
+            id: targetID,
+          },
+          data: {
+            friendIDs: target.friendIDs,
+          },
+        })
+        .catch((err) => console.log(err));
+
+      io.emit("friendDataUpdated", {
+        playerID: initiatorID,
+        friendData: initiator,
+      });
+
+      io.emit("friendDataUpdated", {
+        playerID: targetID,
+        friendData: target,
+      });
+    } else if (
+      (action === "createRoom" || action === "joinRoom") &&
+      roomCode !== undefined &&
+      currentRoomIsPublic !== undefined
+    ) {
+      prisma.user
+        .update({
+          where: {
+            id: initiatorID,
+          },
+          data: {
+            status: "in a room",
+            roomCode: roomCode,
+            currentRoomIsPublic: currentRoomIsPublic,
+          },
+        })
+        .catch((err) => console.log(err));
+
+      for (const friendID of initiator.friendIDs) {
+        const friend = friendData[friendID];
+
+        if (!friend) continue; // don't think this should ever happen but continue better than return here?
+
+        io.emit("friendDataUpdated", {
+          playerID: friendID,
+          friendData: friend,
+        });
+      }
+    } else if (action === "startGame") {
+      prisma.user
+        .update({
+          where: {
+            id: initiatorID,
+          },
+          data: {
+            status: "in a game",
+          },
+        })
+        .catch((err) => console.log(err));
+
+      for (const friendID of initiator.friendIDs) {
+        const friend = friendData[friendID];
+
+        if (!friend) continue; // don't think this should ever happen but continue better than return here?
+
+        io.emit("friendDataUpdated", {
+          playerID: friendID,
+          friendData: friend,
+        });
+      }
+    } else if (action === "leaveRoom") {
+      prisma.user
+        .update({
+          where: {
+            id: initiatorID,
+          },
+          data: {
+            status: "on home screen",
+            roomCode: null,
+            currentRoomIsPublic: null,
+          },
+        })
+        .catch((err) => console.log(err));
+
+      for (const friendID of initiator.friendIDs) {
+        const friend = friendData[friendID];
+
+        if (!friend) continue; // don't think this should ever happen but continue better than return here?
+
+        io.emit("friendDataUpdated", {
+          playerID: friendID,
+          friendData: friend,
+        });
+      }
+    } else if (action === "goOffline") {
+      prisma.user
+        .update({
+          where: {
+            id: initiatorID,
+          },
+          data: {
+            online: false,
+            status: "on home screen",
+            roomCode: null,
+            currentRoomIsPublic: null,
+          },
+        })
+        .catch((err) => console.log(err));
+
+      for (const friendID of initiator.friendIDs) {
+        const friend = friendData[friendID];
+
+        if (!friend) continue; // don't think this should ever happen but continue better than return here?
+
+        io.emit("friendDataUpdated", {
+          playerID: friendID,
+          friendData: friend,
+        });
+      }
+    } else if (action === "goOnline") {
+      prisma.user
+        .update({
+          where: {
+            id: initiatorID,
+          },
+          data: {
+            online: true,
+          },
+        })
+        .catch((err) => console.log(err));
 
       for (const friendID of initiator.friendIDs) {
         const friend = friendData[friendID];
