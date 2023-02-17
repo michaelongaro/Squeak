5351671249bc771765ce1b78da04087b5f55109c
Michael Ongaro <35980684+michaelongaro@users.noreply.github.com>
2023-02-16 23:48:30 -0600

    getLeaderboardStats function

--- src/server/trpc/router/_app.ts
+++ src/server/trpc/router/_app.ts
@@ -1,10 +1,12 @@
 import { usersRouter } from "./users";
 import { router } from "../trpc";
 import { roomsRouter } from "./rooms";
+import { statsRouter } from "./stats";
 
 export const appRouter = router({
   rooms: roomsRouter,
   users: usersRouter,
+  stats: statsRouter,
 });
 
 // export type definition of API
--- src/server/trpc/router/users.ts
+++ src/server/trpc/router/users.ts
@@ -1,6 +1,17 @@
 import { z } from "zod";
 import { router, publicProcedure, protectedProcedure } from "../trpc";
 
+interface IFormattedStats {
+  [category: string]: IFormattedStat[];
+}
+
+interface IFormattedStat {
+  username: string;
+  color: string;
+  avatarPath: string;
+  value: number;
+}
+
 export const usersRouter = router({
   getUserByID: publicProcedure // maybe go protected if it still will return null on "" input
     .input(z.string())
@@ -70,4 +81,101 @@ export const usersRouter = router({
         console.log(error);
       }
     }),
+
+  getLeaderboardStats: publicProcedure.query(async ({ ctx }) => {
+    try {
+      const allUsers = await ctx.prisma.user.findMany({
+        select: {
+          username: true,
+          color: true,
+          avatarPath: true,
+          stats: true,
+        },
+      });
+
+      const leaderboardStats: IFormattedStats = {
+        "Total Squeaks": [],
+        "Average place per round": [],
+        "Average left in Squeak": [],
+        "Highest score": [],
+        "Lowest score": [],
+        "Total games played": [],
+      };
+
+      // plugging in all the stats from each user into the leaderboardStats object
+      allUsers.forEach((user) => {
+        const baseUserMetadata = {
+          username: user.username,
+          color: user.color,
+          avatarPath: user.avatarPath,
+        };
+
+        leaderboardStats["Total Squeaks"]?.push({
+          ...baseUserMetadata,
+          value: user?.stats?.squeaks || 0,
+        });
+        leaderboardStats["Average place per round"]?.push({
+          ...baseUserMetadata,
+          value: user?.stats?.averageFinishingPlace || 0,
+        });
+        leaderboardStats["Average left in Squeak"]?.push({
+          ...baseUserMetadata,
+          value: user?.stats?.averageLeftInSqueak || 0,
+        });
+        leaderboardStats["Highest score"]?.push({
+          ...baseUserMetadata,
+          value: user?.stats?.highestScore || 0,
+        });
+        leaderboardStats["Lowest score"]?.push({
+          ...baseUserMetadata,
+          value: user?.stats?.lowestScore || 0,
+        });
+        leaderboardStats["Total games played"]?.push({
+          ...baseUserMetadata,
+          value: user?.stats?.totalGamesPlayed || 0,
+        });
+      });
+
+      // sorting each stat category by value appropriately
+      Object.keys(leaderboardStats).forEach((category) => {
+        // not sure if best way to verify that that key exists
+        // in the object
+        let currentlyCategory = leaderboardStats[category];
+
+        if (currentlyCategory === undefined) return;
+
+        if (category === "Total Squeaks") {
+          currentlyCategory = currentlyCategory
+            .sort((a, b) => b.value + a.value)
+            .slice(0, 10);
+        } else if (category === "Average place per round") {
+          currentlyCategory = currentlyCategory
+            .sort((a, b) => a.value - b.value)
+            .slice(0, 10);
+        } else if (category === "Average left in Squeak") {
+          currentlyCategory = currentlyCategory
+            .sort((a, b) => b.value - a.value)
+            .slice(0, 10);
+        } else if (category === "Highest score") {
+          currentlyCategory = currentlyCategory
+            .sort((a, b) => b.value + a.value)
+            .slice(0, 10);
+        } else if (category === "Lowest score") {
+          currentlyCategory = currentlyCategory
+            .sort((a, b) => a.value - b.value)
+            .slice(0, 10);
+        } else if (category === "Total games played") {
+          currentlyCategory = currentlyCategory
+            .sort((a, b) => b.value + a.value)
+            .slice(0, 10);
+        }
+
+        leaderboardStats[category] = currentlyCategory;
+      });
+
+      return leaderboardStats;
+    } catch (error) {
+      console.log(error);
+    }
+  }),
 });
