23a677524e8e44dba72994612bd5af164dd5d259
Michael Ongaro <35980684+michaelongaro@users.noreply.github.com>
2023-02-19 03:51:19 -0600

    countdown timer works 95% correctly

--- src/components/modals/ShufflingCountdownModal.tsx
+++ src/components/modals/ShufflingCountdownModal.tsx
@@ -4,6 +4,7 @@ import { useRoomContext } from "../../context/RoomContext";
 import Card from "../Play/Card";
 import AnimatedNumber from "react-awesome-animated-number";
 import { useUserIDContext } from "../../context/UserIDContext";
+import { motion } from "framer-motion";
 
 function ShufflingCountdownModal() {
   const {
@@ -26,11 +27,11 @@ function ShufflingCountdownModal() {
     // timers are offset by 500ms to allow for the animation to play out
     setTimeout(() => {
       setCountdownTimerValue(2);
-    }, 1500);
+    }, 1000);
 
     setTimeout(() => {
       setCountdownTimerValue(1);
-    }, 2500);
+    }, 2000);
 
     if (gameData.currentRound !== 1) {
       setTimeout(() => {
@@ -43,13 +44,13 @@ function ShufflingCountdownModal() {
           roomCode: roomConfig.code,
           firstRound: gameData.currentRound === 1,
         });
-      }, 3500);
+      }, 3000);
     }
 
     setTimeout(() => {
       setShowShufflingCountdown(false);
       setTimersInitiated(false);
-    }, 4000);
+    }, 3500); // full 4000ms delay felt too long
   }, [
     gameData.currentRound,
     roomConfig.code,
@@ -59,59 +60,58 @@ function ShufflingCountdownModal() {
   ]);
 
   return (
-    <div
-      style={{
-        // prob move this to motion.div and have show && (component) with AnimatePresence handle
-        // need for opacity and then pointerEvents won't be necessary because it won't be rendered
-        opacity: showShufflingCountdown ? 1 : 0,
-        pointerEvents: showShufflingCountdown ? "auto" : "none",
-      }}
+    <motion.div
+      key={"shufflingCountdownModal"}
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 0.15 }}
       className="baseFlex absolute top-0 left-0 z-[999] h-full w-full bg-black bg-opacity-60 transition-all"
     >
-      {/* prob will run into issues and will need framer motion to smoothly animate in/out */}
-      {showShufflingCountdown && (
-        <div className="h-fit w-fit rounded-md bg-green-200 p-8 pl-16 pr-16 shadow-md">
-          <div className="baseVertFlex gap-6">
-            <div className="text-xl">Shuffling decks</div>
+      <div className="h-fit w-fit rounded-md bg-green-200 p-8 pl-16 pr-16 shadow-md">
+        <div className="baseVertFlex gap-6">
+          <div className="text-xl">Shuffling decks</div>
 
-            <div className="relative mt-16 h-[64px] w-[48px] tall:h-[87px] tall:w-[67px]">
-              <div className="absolute top-0 left-0 h-full w-full">
-                <Card
-                  showCardBack={true}
-                  draggable={false}
-                  rotation={0}
-                  ownerID={userID}
-                />
-              </div>
-              <div
-                style={{
-                  animationPlayState: "running",
-                }}
-                className={`topBackFacingCardInDeck absolute top-0 left-0 h-full w-full`}
-              >
-                <Card
-                  showCardBack={true}
-                  draggable={false}
-                  rotation={0}
-                  ownerID={userID}
-                />
-              </div>
+          <div className="relative mt-16 h-[64px] w-[48px] tall:h-[87px] tall:w-[67px]">
+            <div className="absolute top-0 left-0 h-full w-full">
+              <Card
+                showCardBack={true}
+                draggable={false}
+                rotation={0}
+                ownerID={userID}
+              />
             </div>
-
-            <div className="baseFlex gap-2">
-              <div>Round will begin in:</div>
-
-              <AnimatedNumber
-                value={countdownTimerValue}
-                duration={1500}
-                hasComma={true}
-                size={20}
+            <div
+              style={{
+                animationPlayState: "running",
+              }}
+              className={`topBackFacingCardInDeck absolute top-0 left-0 h-full w-full`}
+            >
+              <Card
+                showCardBack={true}
+                draggable={false}
+                rotation={0}
+                ownerID={userID}
               />
             </div>
           </div>
+
+          <div className="baseFlex gap-2">
+            <div>Round will begin in:</div>
+
+            <AnimatedNumber
+              value={countdownTimerValue}
+              // needed because by default <AnimatedNumber /> will try to count up from 0
+              // but from the moment this component is rendered, we don't want to show any
+              // counting up, just the countdown.
+              duration={timersInitiated ? 1000 : 0}
+              hasComma={true}
+              size={20}
+            />
+          </div>
         </div>
-      )}
-    </div>
+      </div>
+    </motion.div>
   );
 }
 
