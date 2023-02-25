b4c8a22bae9e038e99c3863ba5c2d0411a473137
Michael Ongaro <35980684+michaelongaro@users.noreply.github.com>
2023-02-25 03:53:40 -0600

    added open graph meta info into <NextSeo />

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import HomePage from "../components/HomePage/HomePage";
 import { io } from "socket.io-client";
 import useReceiveFriendData from "../hooks/useReceiveFriendData";
 import useInitializeUserStats from "../hooks/useInitializeUserStats";
+import { NextSeo } from "next-seo";
 
 export const socket = io({
   path: "/api/socket",
@@ -25,6 +26,27 @@ const Home: NextPage = () => {
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
+      <NextSeo
+        title="Squeak"
+        description="Welcome to Squeak! A fun, fast-paced multiplayer rendition of solitaire. Games can be played with 2-4 players, lasting around 20 minutes."
+        canonical="www.playsqueak.com"
+        openGraph={{
+          url: "www.playsqueak.com",
+          title: "Squeak",
+          description:
+            "Welcome to Squeak! A fun, fast-paced multiplayer rendition of solitaire. Games can be played with 2-4 players, lasting around 20 minutes.",
+          images: [
+            {
+              url: "https://i.imgur.com/EpwvpXH.png",
+              width: 850,
+              height: 650,
+              alt: "Screenshot of a Squeak game",
+            },
+          ],
+          site_name: "Squeak",
+        }}
+      />
       <HomePage />
     </>
   );
