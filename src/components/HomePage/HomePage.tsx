80266e928b148eca96e9ca6c758c31100f171dbc
Michael Ongaro <35980684+michaelongaro@users.noreply.github.com>
2023-02-28 20:40:34 -0600

    shows warning to mobile users

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import CreateRoom from "../CreateRoom/CreateRoom";
 import JoinRoom from "../JoinRoom/JoinRoom";
 import Play from "../Play/Play";
@@ -6,9 +7,25 @@ import MainOptions from "../MainOptions/MainOptions";
 import { AnimatePresence } from "framer-motion";
 import TopRightControls from "../TopRightControls/TopRightControls";
 import usePlayerLeftRoom from "../../hooks/usePlayerLeftRoom";
+import MobileWarningModal from "../modals/MobileWarningModal";
+import { isMobile } from "react-device-detect";
 
 function HomePage() {
   const { connectedToRoom, pageToRender } = useRoomContext();
+
+  const [allowedToShowMobileWarningModal, setAllowedToShowMobileWarningModal] =
+    useState<boolean>(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      if (
+        isMobile &&
+        localStorage.getItem("allowedToShowMobileWarningModal") !== "false"
+      ) {
+        setAllowedToShowMobileWarningModal(true);
+      }
+    }, 1500);
+  }, []);
 
   usePlayerLeftRoom();
 
@@ -24,6 +41,19 @@ function HomePage() {
         {pageToRender === "joinRoom" && <JoinRoom />}
         {pageToRender === "play" && connectedToRoom && <Play />}
       </AnimatePresence>
+
+      <AnimatePresence
+        initial={false}
+        mode={"wait"}
+        onExitComplete={() => null}
+      >
+        {allowedToShowMobileWarningModal && (
+          <MobileWarningModal
+            setShowModal={setAllowedToShowMobileWarningModal}
+          />
+        )}
+      </AnimatePresence>
+
       <TopRightControls forPlayScreen={pageToRender === "play"} />
     </div>
   );
