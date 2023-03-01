df4f561911b4178320aa3b459b927c15ddba3693
Michael Ongaro <35980684+michaelongaro@users.noreply.github.com>
2023-02-28 20:40:18 -0600

    better spacing on change username modal

--- src/components/JoinRoom/JoinRoom.tsx
+++ src/components/JoinRoom/JoinRoom.tsx
@@ -214,7 +214,7 @@ function JoinRoom() {
                         exit={{ opacity: 0 }}
                         transition={{ duration: 0.15 }}
                         style={{
-                          right: "-235px",
+                          right: "-255px",
                           color: "hsl(120deg 100% 86%)",
                         }}
                         className="baseVertFlex absolute top-0 gap-2 rounded-md border-2 border-red-700 bg-green-700 pt-2 pb-2 pr-1 pl-1 shadow-md"
