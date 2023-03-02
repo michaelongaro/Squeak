0e2762cca58613e6c95192e802d06b97664b2081
Michael Ongaro <35980684+michaelongaro@users.noreply.github.com>
2023-03-02 17:53:44 -0600

    staggers reset of gameData and start of shuffling anim

--- src/hooks/useScoreboardData.ts
+++ src/hooks/useScoreboardData.ts
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { useRoomContext } from "../context/RoomContext";
+import { useUserIDContext } from "../context/UserIDContext";
 import { socket } from "../pages";
 import { type IScoreboardMetadata } from "../pages/api/handlers/roundOverHandler";
 
@@ -11,6 +12,8 @@ function useScoreboardData() {
     setPlayerIDWhoSqueaked,
     setScoreboardMetadata,
   } = useRoomContext();
+
+  const { value: userID } = useUserIDContext();
 
   const [dataFromBackend, setDataFromBackend] =
     useState<IScoreboardMetadata | null>(null);
@@ -27,7 +30,7 @@ function useScoreboardData() {
     if (dataFromBackend !== null) {
       setDataFromBackend(null);
 
-      const { gameWinnerID, roundWinnerID, gameData, playerRoundDetails } =
+      const { gameWinnerID, roundWinnerID, playerRoundDetails } =
         dataFromBackend;
 
       setPlayerIDWhoSqueaked(roundWinnerID);
@@ -40,23 +43,23 @@ function useScoreboardData() {
         });
 
         setShowScoreboard(true);
-
-        setGameData(gameData);
       }, 1000); // waiting for pulsing animation to finish
 
       setTimeout(() => {
+        setPlayerIDWhoSqueaked(null);
+        if (roundWinnerID !== userID) return;
+
         socket.emit("resetGame", {
           roomCode: roomConfig.code,
           resettingRoundFromExcessiveDeckRotations: false,
           gameIsFinished: gameWinnerID !== null,
         });
-
-        setPlayerIDWhoSqueaked(null);
-      }, 15000);
+      }, 13000);
     }
   }, [
     dataFromBackend,
     roomConfig,
+    userID,
     setGameData,
     setPlayerIDWhoSqueaked,
     setScoreboardMetadata,
--- src/hooks/useStartAnotherRoundHandler.ts
+++ src/hooks/useStartAnotherRoundHandler.ts
@@ -4,12 +4,17 @@ import { socket } from "../pages";
 import { type IGameMetadata } from "../pages/api/socket";
 
 function useStartAnotherRoundHandler() {
-  const { setGameData, setShowScoreboard, setShowShufflingCountdown } =
-    useRoomContext();
+  const {
+    setGameData,
+    setShowScoreboard,
+    setShowShufflingCountdown,
+    setPlayerIDToStartNextRound,
+  } = useRoomContext();
 
-  const [dataFromBackend, setDataFromBackend] = useState<IGameMetadata | null>(
-    null
-  );
+  const [dataFromBackend, setDataFromBackend] = useState<{
+    gameData: IGameMetadata;
+    playerIDToStartNextRound: string;
+  } | null>(null);
 
   useEffect(() => {
     socket.on("startNewRound", (data) => setDataFromBackend(data));
@@ -23,18 +28,22 @@ function useStartAnotherRoundHandler() {
     if (dataFromBackend !== null) {
       setDataFromBackend(null);
 
-      setGameData(dataFromBackend);
+      setGameData(dataFromBackend.gameData);
+      setPlayerIDToStartNextRound(dataFromBackend.playerIDToStartNextRound);
 
-      setShowScoreboard(false);
+      setTimeout(() => {
+        setShowScoreboard(false);
+      }, 2000);
 
       setTimeout(() => {
         setShowShufflingCountdown(true);
-      }, 250); // might be too long tbh
+      }, 2250); // might be too long tbh
     }
   }, [
     dataFromBackend,
     setShowScoreboard,
     setShowShufflingCountdown,
+    setPlayerIDToStartNextRound,
     setGameData,
   ]);
 }
