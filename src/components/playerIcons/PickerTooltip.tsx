d99115a702d75a3db463cd808a1aacdc23555efb
Michael Ongaro <35980684+michaelongaro@users.noreply.github.com>
2023-03-04 13:35:28 -0600

    fixed key for motion.div

--- src/components/playerIcons/PickerTooltip.tsx
+++ src/components/playerIcons/PickerTooltip.tsx
@@ -340,7 +340,7 @@ function PickerTooltip({
           {/* preview */}
           {playerMetadata && (
             <motion.div
-              key={"tutorialModal"}
+              key={"pickerTooltip"}
               initial={{ opacity: 0, scale: 0 }}
               animate={{ opacity: 1, scale: 1 }}
               exit={{ opacity: 0, scale: 0 }}
