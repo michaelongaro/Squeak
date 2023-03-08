75044d0156c95aa8536707c20924e39038370ebe
Michael Ongaro <35980684+michaelongaro@users.noreply.github.com>
2023-03-08 17:14:15 -0600

    only updates state if value has changed

--- src/hooks/useTrackHoverOverBoardCells.ts
+++ src/hooks/useTrackHoverOverBoardCells.ts
@@ -2,14 +2,14 @@ import { useEffect } from "react";
 import { useRoomContext } from "../context/RoomContext";
 
 function useTrackHoverOverBoardCells() {
-  const { setHoveredCell } = useRoomContext();
+  const { hoveredCell, setHoveredCell } = useRoomContext();
 
   useEffect(() => {
     // mousemove listener to check which squeak stack is being hovered over
     // excludes the squeak stack that the card is currently in
 
     function mouseHandler(e: MouseEvent) {
-      let hoveredCell: [number, number] | null = null;
+      let newHoveredCell: [number, number] | null = null;
 
       for (let i = 0; i < 4; i++) {
         for (let j = 0; j < 5; j++) {
@@ -24,13 +24,20 @@ function useTrackHoverOverBoardCells() {
               e.clientY > cell.top &&
               e.clientY < cell.bottom
             ) {
-              hoveredCell = [i, j];
+              newHoveredCell = [i, j];
             }
           }
         }
       }
 
-      setHoveredCell(hoveredCell);
+      // only update hoveredCell if it has changed
+      if (
+        (hoveredCell === null && newHoveredCell !== null) ||
+        hoveredCell?.[0] !== newHoveredCell?.[0] ||
+        hoveredCell?.[1] !== newHoveredCell?.[1]
+      ) {
+        setHoveredCell(newHoveredCell);
+      }
     }
 
     window.addEventListener("mousemove", mouseHandler);
@@ -38,7 +45,7 @@ function useTrackHoverOverBoardCells() {
     return () => {
       window.removeEventListener("mousemove", mouseHandler);
     };
-  }, [setHoveredCell]);
+  }, [hoveredCell, setHoveredCell]);
 }
 
 export default useTrackHoverOverBoardCells;
