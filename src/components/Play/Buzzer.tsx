f49c311b21f8330e42c4529213ef3442a1c9e472
Michael Ongaro <35980684+michaelongaro@users.noreply.github.com>
2023-03-20 03:49:24 -0500

    not necessary to have exit animation

--- src/components/Play/Buzzer.tsx
+++ src/components/Play/Buzzer.tsx
@@ -65,7 +65,6 @@ function Buzzer({ playerID, roomCode, interactive }: IBuzzer) {
       key={`${playerID}Buzzer`}
       initial={{ opacity: 0, scale: 0 }}
       animate={{ opacity: 1, scale: 1 }}
-      exit={{ opacity: 0, scale: 0 }}
       transition={{ duration: 0.25 }}
       style={{
         boxShadow: hoveringOnButton
