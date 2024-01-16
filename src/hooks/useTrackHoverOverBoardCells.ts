975cf9951a8b92bf8e97ff7f1d93dc6700a939cd
Michael Ongaro <35980684+michaelongaro@users.noreply.github.com>
2024-01-15 23:54:04 -0600

    perf: only fires when holding deck/squeak card

--- src/hooks/useTrackHoverOverBoardCells.ts
+++ src/hooks/useTrackHoverOverBoardCells.ts
@@ -2,13 +2,16 @@ import { useEffect } from "react";
 import { useRoomContext } from "../context/RoomContext";
 
 function useTrackHoverOverBoardCells() {
-  const { hoveredCell, setHoveredCell } = useRoomContext();
+  const { holdingADeckCard, holdingASqueakCard, hoveredCell, setHoveredCell } =
+    useRoomContext();
 
   useEffect(() => {
     // mousemove listener to check which squeak stack is being hovered over
     // excludes the squeak stack that the card is currently in
 
     function mouseHandler(e: MouseEvent) {
+      if (!holdingADeckCard && !holdingASqueakCard) return;
+
       let newHoveredCell: [number, number] | null = null;
 
       for (let i = 0; i < 4; i++) {
@@ -45,7 +48,7 @@ function useTrackHoverOverBoardCells() {
     return () => {
       window.removeEventListener("mousemove", mouseHandler);
     };
-  }, [hoveredCell, setHoveredCell]);
+  }, [hoveredCell, setHoveredCell, holdingADeckCard, holdingASqueakCard]);
 }
 
 export default useTrackHoverOverBoardCells;
