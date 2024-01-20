a8c5ea421e0c6afaf5a0faec468c9c7117cd6355
Michael Ongaro <35980684+michaelongaro@users.noreply.github.com>
2024-01-20 13:31:21 -0600

    easing for "+1" is now ease-in-out

--- src/components/Play/BoardCell.tsx
+++ src/components/Play/BoardCell.tsx
@@ -48,7 +48,7 @@ function BoardCell({ card, rowIdx, colIdx }: IBoardCell) {
               initial={{ opacity: 0, scale: 0.95, translateY: 10 }}
               animate={{ opacity: 1, scale: 1, translateY: 0 }}
               exit={{ opacity: 0, scale: 0.95, translateY: -5 }}
-              transition={{ duration: 0.5 }}
+              transition={{ duration: 0.5, ease: "easeInOut" }}
               style={{
                 color: "hsl(120deg 100% 86%)",
               }}
