d6ea588b7e906641000d888ea8e92b89e748bc24
Michael Ongaro <35980684+michaelongaro@users.noreply.github.com>
2024-02-14 22:25:24 -0600

    waits an extra 1.5s to start dealing cards to wait for users to load /game page

--- src/pages/api/handlers/startGameHandler.ts
+++ src/pages/api/handlers/startGameHandler.ts
@@ -62,50 +62,55 @@ export function startGameHandler(
       const currentRoomPlayers = roomData[roomCode]?.players;
       if (!currentRoomPlayers) return;
 
-      for (const index in Object.keys(currentRoomPlayers)) {
-        const playerID = Object.keys(currentRoomPlayers)[parseInt(index)];
-        if (playerID === undefined) return;
+      setTimeout(
+        () => {
+          for (const index in Object.keys(currentRoomPlayers)) {
+            const playerID = Object.keys(currentRoomPlayers)[parseInt(index)];
+            if (playerID === undefined) return;
 
-        setTimeout(() => {
-          drawFromSqueakDeck({
-            indexToDrawTo: 0,
-            playerID,
-            roomCode,
-            gameData,
-            io,
-          });
-        }, 1500 + parseInt(index) * 400);
+            setTimeout(() => {
+              drawFromSqueakDeck({
+                indexToDrawTo: 0,
+                playerID,
+                roomCode,
+                gameData,
+                io,
+              });
+            }, 1500 + parseInt(index) * 400);
 
-        setTimeout(() => {
-          drawFromSqueakDeck({
-            indexToDrawTo: 1,
-            playerID,
-            roomCode,
-            gameData,
-            io,
-          });
-        }, 2000 + parseInt(index) * 400);
+            setTimeout(() => {
+              drawFromSqueakDeck({
+                indexToDrawTo: 1,
+                playerID,
+                roomCode,
+                gameData,
+                io,
+              });
+            }, 2000 + parseInt(index) * 400);
 
-        setTimeout(() => {
-          drawFromSqueakDeck({
-            indexToDrawTo: 2,
-            playerID,
-            roomCode,
-            gameData,
-            io,
-          });
-        }, 2500 + parseInt(index) * 400);
+            setTimeout(() => {
+              drawFromSqueakDeck({
+                indexToDrawTo: 2,
+                playerID,
+                roomCode,
+                gameData,
+                io,
+              });
+            }, 2500 + parseInt(index) * 400);
 
-        setTimeout(() => {
-          drawFromSqueakDeck({
-            indexToDrawTo: 3,
-            playerID,
-            roomCode,
-            gameData,
-            io,
-          });
-        }, 3000 + parseInt(index) * 400);
-      }
+            setTimeout(() => {
+              drawFromSqueakDeck({
+                indexToDrawTo: 3,
+                playerID,
+                roomCode,
+                gameData,
+                io,
+              });
+            }, 3000 + parseInt(index) * 400);
+          }
+        },
+        firstRound ? 1500 : 0
+      ); // allows for page to be fully* loaded when first navigating to play screen
 
       // start interval that checks + handles if game is stuck
       // (no player has a valid move available)
@@ -113,41 +118,44 @@ export function startGameHandler(
 
       if (!miscRoomDataObj) return;
 
-      setTimeout(() => {
-        for (const index in Object.keys(currentRoomPlayers)) {
-          const playerID = Object.keys(currentRoomPlayers)[parseInt(index)];
-          const player = currentRoomPlayers[playerID || ""];
-          const botDifficulty = player?.botDifficulty;
+      setTimeout(
+        () => {
+          for (const index in Object.keys(currentRoomPlayers)) {
+            const playerID = Object.keys(currentRoomPlayers)[parseInt(index)];
+            const player = currentRoomPlayers[playerID || ""];
+            const botDifficulty = player?.botDifficulty;
 
-          if (!player || !playerID || botDifficulty === undefined) continue;
+            if (!player || !playerID || botDifficulty === undefined) continue;
 
-          let botInterval: NodeJS.Timeout | null = null;
-          setTimeout(() => {
-            // TODO: could probably move this directly to happen when bot joins a room in joinHandler.ts
-            if (
-              miscRoomDataObj.blacklistedSqueakCards[playerID] === undefined
-            ) {
-              miscRoomDataObj.blacklistedSqueakCards[playerID] = {};
-            }
+            let botInterval: NodeJS.Timeout | null = null;
+            setTimeout(() => {
+              // TODO: could probably move this directly to happen when bot joins a room in joinHandler.ts
+              if (
+                miscRoomDataObj.blacklistedSqueakCards[playerID] === undefined
+              ) {
+                miscRoomDataObj.blacklistedSqueakCards[playerID] = {};
+              }
 
-            botInterval = setInterval(
-              () =>
-                botMoveHandler(
-                  io,
-                  roomCode,
-                  gameData,
-                  roomData,
-                  miscRoomData,
-                  playerID
-                ),
-              botDifficultyDelay[botDifficulty] +
-                (Math.floor(Math.random() * 1000) - 500) // random offset by +- 500ms
-            );
+              botInterval = setInterval(
+                () =>
+                  botMoveHandler(
+                    io,
+                    roomCode,
+                    gameData,
+                    roomData,
+                    miscRoomData,
+                    playerID
+                  ),
+                botDifficultyDelay[botDifficulty] +
+                  (Math.floor(Math.random() * 1000) - 500) // random offset by +- 500ms
+              );
 
-            if (botInterval) miscRoomDataObj.botIntervals.push(botInterval);
-          }, 1500 * parseInt(index)); // TODO: still test out better variations with delay..
-        }
-      }, 7000); // roughly the time it takes for the cards to be dealt to the players on client side
+              if (botInterval) miscRoomDataObj.botIntervals.push(botInterval);
+            }, 1500 * parseInt(index)); // TODO: still test out better variations with delay..
+          }
+        },
+        firstRound ? 8500 : 7000
+      ); // roughly the time it takes for the cards to be dealt to the players on client side
 
       if (firstRound && prisma) {
         io.in(roomCode).emit("navigateToPlayScreen", gameData[roomCode]);
