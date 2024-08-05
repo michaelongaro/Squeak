040c8da1af963e00f8e4ff4ec2f9cdc47175752f
Michael Ongaro <35980684+michaelongaro@users.noreply.github.com>
2024-08-05 03:39:36 -0500

    trying to further guarentee plusOne indicator animation getting "flushed out"

--- src/components/Play/BoardCell.tsx
+++ src/components/Play/BoardCell.tsx
@@ -2,6 +2,7 @@ import { useRoomContext } from "../../context/RoomContext";
 import Card from "./Card";
 import { AnimatePresence, motion } from "framer-motion";
 import { type ICard } from "../../utils/generateDeckAndSqueakCards";
+import { useEffect, useState } from "react";
 
 interface IBoardCell {
   card: ICard | null;
@@ -18,6 +19,28 @@ export interface IGetBoxShadowStyles {
 
 function BoardCell({ card, rowIdx, colIdx }: IBoardCell) {
   const { proposedCardBoxShadow } = useRoomContext();
+
+  const [showPlusOneIndicator, setShowPlusOneIndicator] = useState(false);
+
+  useEffect(() => {
+    if (
+      proposedCardBoxShadow !== null &&
+      proposedCardBoxShadow.id === `cell${rowIdx}${colIdx}` &&
+      proposedCardBoxShadow.boxShadowValue ===
+        "0px 0px 4px 3px hsl(120, 100%, 86%)"
+    ) {
+      setShowPlusOneIndicator(true);
+
+      setTimeout(() => {
+        setShowPlusOneIndicator(false);
+      }, 500); // trying to make sure the animation completes before hiding the indicator
+      // this has been a recurring problem despite multiple approaches to fix it
+    }
+
+    return () => {
+      setShowPlusOneIndicator(false);
+    };
+  }, [proposedCardBoxShadow, colIdx, rowIdx]);
 
   return (
     <AnimatePresence>
@@ -40,21 +63,18 @@ function BoardCell({ card, rowIdx, colIdx }: IBoardCell) {
       )}
 
       <AnimatePresence>
-        {proposedCardBoxShadow !== null &&
-          proposedCardBoxShadow.id === `cell${rowIdx}${colIdx}` &&
-          proposedCardBoxShadow.boxShadowValue ===
-            "0px 0px 4px 3px hsl(120, 100%, 86%)" && (
-            <motion.div
-              key={`board${rowIdx}${colIdx}AnimatedPlusOneIndicator-${proposedCardBoxShadow.boxShadowValue}`}
-              initial={{ opacity: 0, scale: 0.95, translateY: 10 }}
-              animate={{ opacity: 1, scale: 1, translateY: 0 }}
-              exit={{ opacity: 0, scale: 0.95, translateY: -5 }}
-              transition={{ duration: 0.5, ease: "easeInOut" }}
-              className="absolute right-[-1.5rem] top-0 select-none text-lg tracking-wider text-lightGreen tablet:right-[-1.85rem] desktop:text-xl"
-            >
-              +1
-            </motion.div>
-          )}
+        {showPlusOneIndicator && (
+          <motion.div
+            key={`board${rowIdx}${colIdx}AnimatedPlusOneIndicator`}
+            initial={{ opacity: 0, scale: 0.95, translateY: 10 }}
+            animate={{ opacity: 1, scale: 1, translateY: 0 }}
+            exit={{ opacity: 0, scale: 0.95, translateY: -5 }}
+            transition={{ duration: 0.5, ease: "easeInOut" }}
+            className="absolute right-[-1.5rem] top-0 select-none text-lg tracking-wider text-lightGreen tablet:right-[-1.85rem] desktop:text-xl"
+          >
+            +1
+          </motion.div>
+        )}
       </AnimatePresence>
     </AnimatePresence>
   );
