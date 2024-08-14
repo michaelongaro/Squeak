b6edfa1bde12224e5875ca3cf42bedd8b95a49d8
Michael Ongaro <35980684+michaelongaro@users.noreply.github.com>
2024-08-13 23:46:47 -0500

    removed animejs dependency by refactoring to use framer-motion + trying shadow-sm on <AnimatedCard />

--- src/components/modals/Scoreboard/AnimatedCardContainer.tsx
+++ src/components/modals/Scoreboard/AnimatedCardContainer.tsx
@@ -1,5 +1,9 @@
-import AnimatedCard from "./AnimatedCard";
 import { type ICard } from "../../../utils/generateDeckAndSqueakCards";
+import React, { useEffect } from "react";
+import { useRoomContext } from "../../../context/RoomContext";
+import Card from "../../Play/Card";
+import { motion, useAnimation } from "framer-motion";
+import { type IRoomPlayersMetadata } from "~/pages/api/socket";
 
 interface IAnimatedCardContainer {
   cards: ICard[];
@@ -7,6 +11,8 @@ interface IAnimatedCardContainer {
 }
 
 function AnimatedCardContainer({ cards, playerID }: IAnimatedCardContainer) {
+  const { playerMetadata } = useRoomContext();
+
   return (
     <div
       id={`scoreboardAnimatedCardContainer${playerID}`}
@@ -19,6 +25,7 @@ function AnimatedCardContainer({ cards, playerID }: IAnimatedCardContainer) {
       {cards.map((card, index) => (
         <AnimatedCard
           key={`${playerID}scoreboardAnimatedCard${index}`}
+          playerMetadata={playerMetadata}
           card={card}
           playerID={playerID}
           index={index}
@@ -30,3 +37,78 @@ function AnimatedCardContainer({ cards, playerID }: IAnimatedCardContainer) {
 }
 
 export default AnimatedCardContainer;
+
+interface IAnimatedCard {
+  playerMetadata: IRoomPlayersMetadata;
+  card: ICard;
+  index: number;
+  playerID: string;
+  totalCardsPlayed: number;
+}
+
+function AnimatedCard({
+  card,
+  index,
+  playerID,
+  totalCardsPlayed,
+}: IAnimatedCard) {
+  const { playerMetadata } = useRoomContext();
+  const controls = useAnimation();
+
+  useEffect(() => {
+    const animatedCardContainer = document
+      .getElementById(`scoreboardAnimatedCardContainer${playerID}`)
+      ?.getBoundingClientRect();
+
+    const animatedCard = document.getElementById(
+      `scoreboardAnimatedCard${index}${playerID}`,
+    );
+
+    if (!animatedCardContainer || !animatedCard) return;
+
+    const middleOfContainer = Math.floor(animatedCardContainer.width / 2);
+    const finalY = Math.floor(animatedCardContainer.height * 0.25) - index;
+
+    // Calculate delay and duration based on totalCardsPlayed
+    const totalAnimationTime = 4000; // Total time (ms) for all cards to be played
+    const delay = (totalAnimationTime / totalCardsPlayed) * index;
+    const duration = totalAnimationTime / totalCardsPlayed / 1000; // convert to seconds
+
+    // Start the animation
+    controls.start({
+      top: finalY,
+      left: middleOfContainer,
+      rotateX: 25,
+      rotateZ: 50,
+      transition: {
+        delay: delay / 1000, // convert to seconds
+        duration,
+        ease: "easeOut",
+      },
+    });
+  }, [playerID, index, totalCardsPlayed, controls]);
+
+  return (
+    <motion.div
+      key={`${playerID}scoreboardAnimatedCard${index}`}
+      id={`scoreboardAnimatedCard${index}${playerID}`}
+      className="absolute shadow-sm"
+      initial={{
+        top: "-100%",
+        left: "50%",
+        x: "-50%",
+        rotateX: 25,
+        rotateZ: 50,
+      }}
+      animate={controls}
+    >
+      <Card
+        value={card.value}
+        suit={card.suit}
+        draggable={false}
+        hueRotation={playerMetadata[playerID]?.deckHueRotation || 0}
+        rotation={0}
+      />
+    </motion.div>
+  );
+}
