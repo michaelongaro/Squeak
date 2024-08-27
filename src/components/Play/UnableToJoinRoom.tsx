276c73a345fa57cbfccbf581714c6d02b8ad46a8
Michael Ongaro <35980684+michaelongaro@users.noreply.github.com>
2024-08-27 18:42:10 -0500

    responsive typography

--- src/components/Play/UnableToJoinRoom.tsx
+++ src/components/Play/UnableToJoinRoom.tsx
@@ -14,10 +14,10 @@ function UnableToJoinRoom({ header, body }: IUnableToJoinRoom) {
     <div className="baseVertFlex min-h-[100dvh] py-16">
       <div className="baseVertFlex w-10/12 gap-4 rounded-md border-2 border-lightGreen bg-gradient-to-br from-green-800 to-green-850 p-4 text-lightGreen md:w-[500px] md:p-8">
         <div className="baseFlex gap-2">
-          <IoWarningOutline className="size-8" />
-          <h1 className="text-2xl font-semibold">{header}</h1>
+          <IoWarningOutline className="size-6 tablet:size-8" />
+          <h1 className="text-lg font-semibold tablet:text-2xl">{header}</h1>
         </div>
-        <p className="text-center text-lg">{body}</p>
+        <p className="text-center tablet:text-lg">{body}</p>
 
         <Button onClick={() => router.push("/")} className="mt-4 gap-3">
           <IoHome size={"1.25rem"} />
