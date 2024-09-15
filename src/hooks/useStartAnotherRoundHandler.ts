f5020bbebc6eb7842758af43c38a7738b171d612
Michael Ongaro <35980684+michaelongaro@users.noreply.github.com>
2024-09-15 13:57:37 -0500

    only listening to hostUserID in effect deps

--- src/hooks/useStartAnotherRoundHandler.ts
+++ src/hooks/useStartAnotherRoundHandler.ts
@@ -54,7 +54,7 @@ function useStartAnotherRoundHandler() {
     setShowScoreboard,
     setShowShufflingCountdown,
     setGameData,
-    roomConfig,
+    roomConfig.hostUserID,
     userID,
   ]);
 }
