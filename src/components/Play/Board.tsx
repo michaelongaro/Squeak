f4ea80dcfc7e9cbe5e1fd71639f995fbd7e60e1f
Michael Ongaro <35980684+michaelongaro@users.noreply.github.com>
2024-09-21 04:18:32 -0500

    moving +1 indicator back inside <BoardCell> so that it respects scaling down wrapping div on placement of a king card

--- src/components/Play/Board.tsx
+++ src/components/Play/Board.tsx
@@ -113,40 +113,8 @@ function Board() {
                   colIdx={colIdx}
                   card={cell}
                   deckVariantIndex={deckVariantIndex}
+                  plusOneIndicatorID={plusOneIndicatorID}
                 />
-
-                {/* TODO: the start of this whole animation
-                feels slightly delayed... maybe I am hallucinating this effect */}
-                <AnimatePresence>
-                  {plusOneIndicatorID === `cell${rowIdx}${colIdx}` && (
-                    <motion.div
-                      key={`cell${rowIdx}${colIdx}AnimatedPlusOneIndicator`}
-                      initial={{ opacity: 0, scale: 0.85 }}
-                      animate={{ opacity: 1, scale: 1 }}
-                      exit={{ opacity: 0 }}
-                      transition={{
-                        duration: 0.75,
-                        ease: "easeOut",
-                      }}
-                      className={`baseFlex absolute left-0 top-0 z-[500] h-full w-full select-none rounded-sm bg-darkGreen/50 text-lg tracking-wider text-lightGreen [text-shadow:_0_1px_3px_rgb(0_0_0)] desktop:text-xl`}
-                    >
-                      <motion.div
-                        initial={{ scale: 0 }}
-                        animate={{ scale: 1 }}
-                        exit={{ scale: 0 }}
-                        transition={{
-                          type: "spring",
-                          damping: 4.5,
-                          stiffness: 100,
-                          delay: 0.25,
-                        }}
-                        className="baseFlex h-full w-full"
-                      >
-                        +1
-                      </motion.div>
-                    </motion.div>
-                  )}
-                </AnimatePresence>
               </div>
             </div>
           ))}
--- src/components/Play/BoardCell.tsx
+++ src/components/Play/BoardCell.tsx
@@ -14,9 +14,16 @@ interface IBoardCell {
   rowIdx: number;
   colIdx: number;
   deckVariantIndex: number;
+  plusOneIndicatorID: string | null;
 }
 
-function BoardCell({ card, rowIdx, colIdx, deckVariantIndex }: IBoardCell) {
+function BoardCell({
+  card,
+  rowIdx,
+  colIdx,
+  deckVariantIndex,
+  plusOneIndicatorID,
+}: IBoardCell) {
   return (
     <AnimatePresence>
       {card && (
@@ -31,6 +38,39 @@ function BoardCell({ card, rowIdx, colIdx, deckVariantIndex }: IBoardCell) {
             value={card.value}
             deckVariantIndex={deckVariantIndex}
           />
+
+          {/* TODO: the start of this whole animation
+                feels slightly delayed... maybe I am hallucinating this effect */}
+          <AnimatePresence>
+            {plusOneIndicatorID === `cell${rowIdx}${colIdx}` && (
+              <motion.div
+                key={`cell${rowIdx}${colIdx}AnimatedPlusOneIndicator`}
+                initial={{ opacity: 0, scale: 0.85 }}
+                animate={{ opacity: 1, scale: 1 }}
+                exit={{ opacity: 0 }}
+                transition={{
+                  duration: 0.75,
+                  ease: "easeOut",
+                }}
+                className={`baseFlex absolute left-0 top-0 z-[500] h-full w-full select-none rounded-sm bg-darkGreen/50 text-lg tracking-wider text-lightGreen [text-shadow:_0_1px_3px_rgb(0_0_0)] desktop:text-xl`}
+              >
+                <motion.div
+                  initial={{ scale: 0 }}
+                  animate={{ scale: 1 }}
+                  exit={{ scale: 0 }}
+                  transition={{
+                    type: "spring",
+                    damping: 4.5,
+                    stiffness: 100,
+                    delay: 0.25,
+                  }}
+                  className="baseFlex h-full w-full"
+                >
+                  +1
+                </motion.div>
+              </motion.div>
+            )}
+          </AnimatePresence>
         </motion.div>
       )}
     </AnimatePresence>
