47357cf5a00481e119fc188c8f3b3e4da9985d1f
Michael Ongaro <michael.ongaro.dev@gmail.com>
2024-11-29 23:28:23 -0600

    set default color of "x" to be darkGreen

--- src/components/ui/sheet.tsx
+++ src/components/ui/sheet.tsx
@@ -67,7 +67,7 @@ const SheetContent = React.forwardRef<
       {...props}
     >
       <div className="baseFlex relative z-[300] h-8 w-full border-b-2 border-darkGreen !bg-zinc-200 shadow-none">
-        <SheetPrimitive.Close className="absolute right-4 top-[7px] rounded-sm ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-secondary">
+        <SheetPrimitive.Close className="absolute right-4 top-[7px] rounded-sm text-darkGreen ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-secondary">
           <X className="h-4 w-4" />
           <span className="sr-only">Close</span>
         </SheetPrimitive.Close>
