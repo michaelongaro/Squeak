0745721a5c7aeaf0873cf0412b11c2617d7b513c
Michael Ongaro <michael.ongaro.dev@gmail.com>
2026-02-07 13:07:19 -0600

    focus ring is now lightGreen

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -13,8 +13,8 @@ const Input = React.forwardRef<
     <input
       type={type}
       className={cn(
-        "flex h-10 rounded-md border-2 border-darkGreen bg-background px-2 py-1 text-darkGreen ring-offset-background transition-all file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-0 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
-        className
+        "flex h-10 rounded-md border bg-background px-2 py-1 text-darkGreen ring-offset-lightGreen transition-all file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-0 focus-visible:ring-darkGreen focus-visible:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50",
+        className,
       )}
       ref={ref}
       {...props}
